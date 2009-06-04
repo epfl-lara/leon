fa482005d86e94ef78f94c5f34f12cc32a4e82c5
Mirco Dotta <mirco.dotta@gmail.com>
2009-06-04 22:45:39 +0000

    - Added Specification for BST that uses ScalaCheck. - Modified "Empty" data type from being declared as "class" to "object"

--- examples/contracts/bstdefspec/BSTAsSet.scala
+++ examples/contracts/bstdefspec/BSTAsSet.scala
@@ -2,22 +2,22 @@ package contracts.bstdefspec
 
 object BSTAsSet extends Application {
   sealed abstract class BST
-  case class Empty() extends BST
+  case object Empty extends BST
   case class Node(left: BST, value: Int, right: BST) extends BST
 
   def content(t : BST) : Set[Int] = t match {
-    case Empty() => Set.empty
+    case Empty => Set.empty
     case Node(left,v,right) => (content(left) ++ Set(v) ++ content(right))
   }
 
   def size(t : BST) : Int = t match {
-    case Empty() => 0
+    case Empty => 0
     case Node(left,v,right) => size(left) + 1 + size(right)
   }
 
   def add(x : Int, t : BST) : BST = {
     t match {
-      case Empty() => Node(Empty(),x,Empty())
+      case Empty => Node(Empty,x,Empty)
       case Node(left,v,right) => {
 	if (x < v) 
 	  Node(add(x, left), v, right)
@@ -30,7 +30,7 @@ object BSTAsSet extends Application {
 
   def contains(key: Int, t : BST): Boolean = { 
     t match {
-      case Empty() => false
+      case Empty => false
       case Node(left,v,right) => {
 	if (key == v) true
 	else if (key < v) contains(key, left)
@@ -41,9 +41,9 @@ object BSTAsSet extends Application {
 
   // executable functions that 'illustrate' the meaning of quantifiers
   def forallTree(property : (BST => Boolean)) : Boolean = {
-    property(Empty()) &&
-    property(Node(Empty(), 3, Empty())) &&
-    property(Node(Node(Empty(), 3, Empty()), 5, Node(Empty(), 7, Empty())))
+    property(Empty) &&
+    property(Node(Empty, 3, Empty)) &&
+    property(Node(Node(Empty, 3, Empty), 5, Node(Empty, 7, Empty)))
     // if it holds for these trees, why wouldn't it hold in general?
   }
   def forallInt(property : (Int => Boolean)) : Boolean = {
@@ -55,7 +55,7 @@ object BSTAsSet extends Application {
   }
 
   // main, only for testing purposes
-  val t = add(17, add(6, add(3, add(5, add(3, Empty())))))
+  val t = add(17, add(6, add(3, add(5, add(3, Empty)))))
   println(t)
   println(content(t))
   println(size(t))
