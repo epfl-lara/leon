d3610bdfddcfc9a6edace64f349f52c866f98e13
Philippe Suter <philippe.suter@gmail.com>
2010-08-27 11:36:38 +0000

    filter seems to be very hard. wonder why.

--- vmcai2011-testcases/ListProperties.scala
+++ vmcai2011-testcases/ListProperties.scala
@@ -5,41 +5,56 @@ object IntListProperties {
   case class Cons(head: Int, tail: IntList) extends IntList
   case class Nil() extends IntList
 
+  case class IntListPair(left: IntList, right: IntList)
+
+  // some uninterpreted predicate on integers
+  def p(i: Int) : Boolean = { throw new Exception("Uninterpreted.") }
+
   def size(list: IntList) : Int = (list match {
     case Nil() => 0
     case Cons(_, xs) => 1 + size(xs)
   }) ensuring(_ >= 0)
 
+  // proved with unrolling = 1
   def content(list: IntList) : Set[Int] = (list match {
     case Nil() => Set.empty[Int]
     case Cons(x, xs) => Set(x) ++ content(xs)
   }) ensuring(_.size <= size(list))
 
-  def removeDuplicates(list: IntList) : IntList = (list match {
-    case Nil() => Nil()
-    case Cons(x, xs) => Cons(x, removeDuplicates(removeFrom(x, xs)))
-  }) ensuring(newList => content(newList) == content(list))
-
-  def removeFrom(v: Int, list: IntList) : IntList = (list match {
+  // should be proved with unrolling = 1
+  def filter(list: IntList) : IntList = (list match {
     case Nil() => Nil()
     case Cons(x, xs) => {
-      val rest = removeFrom(v, xs)
-      if(v == x) rest else Cons(x, rest)
+      val rest = filter(xs)
+      if(p(x)) Cons(x, rest) else rest
     }
-  }) ensuring(newList => content(newList) == content(list) -- Set(v))
+  }) ensuring(res => content(res).size <= (content(list)).size)
 
-//  def reverse(list: IntList) : IntList = reverse0(list, Nil())
-//  def reverse0(list1: IntList, list2: IntList) : IntList = list1 match {
-//    case Nil() => list2
-//    case Cons(x, xs) => reverse0(xs, Cons(x, list2))
-//  }
+//  def removeDuplicates(list: IntList) : IntList = (list match {
+//    case Nil() => Nil()
+//    case Cons(x, xs) => Cons(x, removeDuplicates(removeFrom(x, xs)))
+//  }) ensuring(newList => content(newList) == content(list))
 //
-//  def concat(list1: IntList, list2: IntList) : IntList = concat0(list1, list2, Nil())
-//  def concat0(list1: IntList, list2: IntList, list3: IntList) : IntList = list1 match {
-//    case Nil() => list2 match {
-//      case Nil() => reverse(list3)
-//      case Cons(y, ys) => concat0(Nil(), ys, Cons(y, list3))
+//  // proved with unrolling = 1 (but sloooooooow)
+//  def removeFrom(v: Int, list: IntList) : IntList = (list match {
+//    case Nil() => Nil()
+//    case Cons(x, xs) => {
+//      val rest = removeFrom(v, xs)
+//      if(v == x) rest else Cons(x, rest)
 //    }
-//    case Cons(x, xs) => concat0(xs, list2, Cons(x, list3))
-//  }
+//  }) ensuring(newList => content(newList) == content(list) -- Set(v))
+//
+//  // proved with unrolling = 1
+//  def occurCount(v: Int, list: IntList) : Int = (list match {
+//    case Nil() => 0
+//    case Cons(x, xs) => {
+//      (if(x == v) 1 else 0) + occurCount(v, xs)
+//    }
+//  }) ensuring(count => count >= 0 && (count > 0) == content(list).contains(v))
+//
+//  // proved with unrolling = 1
+//  def occurs(v: Int, list: IntList) : Boolean = (list match {
+//    case Nil() => false
+//    case Cons(x, xs) => if(x == v) true else occurs(v, xs)
+//  }) ensuring(_ == content(list).contains(v))
 }
