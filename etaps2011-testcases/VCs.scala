eda594710ea6cf65854ca75e05756d2e2d51ee22
Philippe Suter <philippe.suter@gmail.com>
2010-09-30 14:11:33 +0000

    more verification conditions (from paper)

--- etaps2011-testcases/VCs.scala
+++ etaps2011-testcases/VCs.scala
@@ -13,6 +13,42 @@ object VCs {
       a.contains(b.size)
   } ensuring(_ == true)
 
+  def forFun2(a: Set[Int], b: Set[Int], n: Int) : Boolean = {
+    require(
+        (a != Set.empty[Int])
+     && (a.min == 1)
+     && (a.max == n)
+     && (a.size == n)
+     && (b subsetOf a)
+     && (b != Set.empty[Int])
+    )
+      a.contains(b.size)
+  } ensuring(_ == true)
+
+  def paperBSTFind(c: Set[Int], l: Set[Int], r: Set[Int], v: Int, range1: Set[Int], range2: Set[Int], range3: Set[Int]) : Boolean = {
+    require(
+         (c == l ++ Set(v) ++ r)
+      && (l.max < v)
+      && (v < r.min)
+      && (range1 ++ range2 ++ range3 == c)
+      && (range1.max < range2.min)
+      && (range2.min < range3.max)
+      && (range1.size == l.size)
+      && (range2.size == 1)
+      && (range3.size == c.size - l.size - 1)
+    )
+      Set(v) == range2
+  } ensuring(_ == true)
+
+  def paperPartitionPivot(above: Set[Int], pivot: Int, e: Int, abovePrime: Set[Int]) : Boolean = {
+    require(
+         (above == Set.empty[Int] || pivot < above.min)
+      && !(e <= pivot)
+      && (abovePrime == above ++ Set(e))
+    )
+      pivot < abovePrime.min
+  } ensuring(_ == true)
+
   def heapSortNoRepeat1(hContent: Set[Int], hMinusMaxContent: Set[Int], recListContent: Set[Int]) : Boolean = {
     require(
          (hContent != Set.empty[Int])
