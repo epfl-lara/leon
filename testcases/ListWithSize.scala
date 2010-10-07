2f1d9ea4e9a82546ab13a7e08d596cf7fbd47e58
Viktor Kuncak <vkuncak@gmail.com>
2010-10-07 15:39:40 +0000

    playing with induction

--- testcases/ListWithSize.scala
+++ testcases/ListWithSize.scala
@@ -17,20 +17,46 @@ object ListWithSize {
     }
 
     // proved with unrolling=1
-    def append(x: Int, l: List) : List = (l match {
+    def funnyCons(x: Int, l: List) : List = (l match {
         case Nil() => Cons(x, Nil())
         case c @ Cons(_,_) => Cons(x, c)
     }) ensuring(size(_) > 0)
 
     // proved with unrolling=2
-    def reverse(l: List) : List = reverse0(l, Nil()) ensuring(content(_) == content(l))
+    def reverse(l: List) : List = reverse0(l, Nil()) // ensuring(content(_) == content(l))
     def reverse0(l1: List, l2: List) : List = l1 match {
       case Nil() => l2
       case Cons(x, xs) => reverse0(xs, Cons(x, l2))
     }
 
+    def append(l1 : List, l2 : List) : List = (l1 match {
+      case Nil() => l2
+      case Cons(x,xs) => Cons(x, append(xs, l2))
+    })
+
+    def propAppend1(l : List) : Boolean = {
+      append(l, Nil()) == l
+    }
+
+    def propAppend2(l : List) : Boolean = (l match {
+      case Nil() => propAppend1(l)
+      case Cons(x,xs) => (!propAppend1(xs) || propAppend1(l))
+    }) ensuring (_ == true)
+
+    def propAppend3(l : List) : Boolean = {
+      !propAppend1(l) || propAppend2(l)
+    } ensuring (_ == true)
+
+/*
+    def induct(l : List, prop : List => Boolean) = {
+      case Nil() => prop(l)
+      case Cons(x,xs) => (!prop(xs) || prop(l))
+    }
+*/
+
     // proved with unrolling=4
-    def concat(l1: List, l2: List) : List = concat0(l1, l2, Nil()) ensuring(content(_) == content(l1) ++ content(l2))
+    def concat(l1: List, l2: List) : List = concat0(l1, l2, Nil()) 
+     // ensuring(content(_) == content(l1) ++ content(l2))
     def concat0(l1: List, l2: List, l3: List) : List = (l1 match {
       case Nil() => l2 match {
         case Nil() => reverse(l3)
