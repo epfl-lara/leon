e0b90e8e5f541a0a0b37ef9acb98efcc2462fd74
Viktor Kuncak <vkuncak@gmail.com>
2010-11-17 11:27:24 +0000

    added test to nnf, re-arranged some functions

--- pldi2011-testcases/NNF.scala
+++ pldi2011-testcases/NNF.scala
@@ -9,26 +9,6 @@ object NNF {
   case class Not(f: Formula) extends Formula
   case class Literal(id: Int) extends Formula
 
-  def nnf(formula: Formula): Formula = formula match {
-    case And(lhs, rhs) => And(nnf(lhs), nnf(rhs))
-    case Or(lhs, rhs) => Or(nnf(lhs), nnf(rhs))
-    case Not(And(lhs, rhs)) => Or(nnf(Not(lhs)), nnf(Not(rhs)))
-    case Not(Or(lhs, rhs)) => And(nnf(Not(lhs)), nnf(Not(rhs)))
-    case Not(Not(f)) => nnf(f)
-    case Not(Literal(_)) => formula
-    case Literal(_) => formula
-  }
-
-  def freeVars(f: Formula): Set[Int] = {
-    require(isNNF(f))
-    f match {
-      case And(lhs, rhs) => freeVars(lhs) ++ freeVars(rhs)
-      case Or(lhs, rhs) => freeVars(lhs) ++ freeVars(rhs)
-      case Not(Literal(i)) => Set[Int](i)
-      case Literal(i) => Set[Int](i)
-    }
-  }
-
   def isNNF(f: Formula): Boolean = f match {
     case And(lhs, rhs) => isNNF(lhs) && isNNF(rhs)
     case Or(lhs, rhs) => isNNF(lhs) && isNNF(rhs)
@@ -36,4 +16,25 @@ object NNF {
     case Not(_) => false
     case Literal(_) => true
   }
+
+  def nnf(formula: Formula): Formula = (formula match {
+    case And(lhs, rhs) => And(nnf(lhs), nnf(rhs))
+    case Or(lhs, rhs) => Or(nnf(lhs), nnf(rhs))
+    case Not(And(lhs, rhs)) => Or(nnf(Not(lhs)), nnf(Not(rhs)))
+    case Not(Or(lhs, rhs)) => And(nnf(Not(lhs)), nnf(Not(rhs)))
+    case Not(Not(f)) => nnf(f)
+    case Not(Literal(_)) => formula
+    case Literal(_) => formula
+  }) ensuring(f => isNNF(f))
+
+  def freeVars(f: Formula): Set[Int] = {
+  require(isNNF(f))
+    f match {
+      case And(lhs, rhs) => freeVars(lhs) ++ freeVars(rhs)
+      case Or(lhs, rhs) => freeVars(lhs) ++ freeVars(rhs)
+      case Not(Literal(i)) => Set[Int](i)
+      case Literal(i) => Set[Int](i)
+    }
+  }
+  def fv(f : Formula) = { freeVars(nnf(f)) }
 }
