72d817f5f7316ec2ca57b5b78516176447fad54f
Philippe Suter <philippe.suter@gmail.com>
2011-03-31 09:55:26 +0000

    stub

--- src/purescala/isabelle/Main.scala
+++ src/purescala/isabelle/Main.scala
@@ -7,11 +7,23 @@ import purescala.Extensions._
 import purescala.Settings
 import purescala.Common.Identifier
 
+import java.io._
+
 class Main(reporter : Reporter) extends Analyser(reporter) {
   val description = "Generates Isabelle source"
   override val shortDescription = "isabelle"
 
   def analyse(program : Program) : Unit = {
+    val file = File.createTempFile("isabelle", ".thy")
+
+    val out = new BufferedWriter(new FileWriter(file))
+
+    out.write("text")
+
+    out.close
+
+    reporter.info("The file name is : " + file.getPath.toString)
+
     reporter.info(program)
     reporter.info("Done with Isabelle translation.")
   }
