71334653561b081599794f6c76d749b4b329030d
Ali Sinan Köksal <alisinan@gmail.com>
2011-04-05 12:34:37 +0000

    Folder for CP example files.

---
+++ cp-demo/ChooseCalls.scala
@@ -0,0 +1,55 @@
+import scala.collection.immutable.Set
+import funcheck.Annotations._
+import funcheck.Utils._
+import cp.CP._
+
+object ChooseCalls { 
+  sealed abstract class Color
+  case class Red() extends Color
+  case class Black() extends Color
+ 
+  sealed abstract class Tree
+  case class Empty() extends Tree
+  case class Node(color: Color, left: Tree, value: Int, right: Tree) extends Tree
+
+  def size(t: Tree) : Int = t match {
+    case Empty() => 0
+    case Node(_, l, v, r) => size(l) + 1 + size(r)
+  }
+
+  def isBlack(t: Tree) : Boolean = t match {
+    case Empty() => true
+    case Node(Black(),_,_,_) => true
+    case _ => false
+  }
+
+  def redNodesHaveBlackChildren(t: Tree) : Boolean = t match {
+    case Empty() => true
+    case Node(Black(), l, _, r) => redNodesHaveBlackChildren(l) && redNodesHaveBlackChildren(r)
+    case Node(Red(), l, _, r) => isBlack(l) && isBlack(r) && redNodesHaveBlackChildren(l) && redNodesHaveBlackChildren(r)
+  }
+
+  def blackBalanced(t : Tree) : Boolean = t match {
+    case Node(_,l,_,r) => blackBalanced(l) && blackBalanced(r) && blackHeight(l) == blackHeight(r)
+    case Empty() => true
+  }
+
+  def blackHeight(t : Tree) : Int = t match {
+    case Empty() => 1
+    case Node(Black(), l, _, _) => blackHeight(l) + 1
+    case Node(Red(), l, _, _) => blackHeight(l)
+  }
+
+  // def ct2(t : Tree) : Boolean = {
+  //   !(blackBalanced(t) && size(t) == 4)
+  // } holds
+
+  def chooseTree(height : Int) : Tree = {
+    choose((t: Tree) => blackBalanced(t) && size(t) == 5)
+  }
+
+  def main(args: Array[String]) : Unit = {
+    val height = 3
+    println("The chosen tree (of height " + height + ") is : " + chooseTree(height))
+  }
+}
