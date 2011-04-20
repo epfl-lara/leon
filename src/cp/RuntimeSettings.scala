783abc099b98e76d4fd9194e16a04e01e3383935
Ali Sinan Köksal <alisinan@gmail.com>
2011-04-20 07:43:33 +0000

    Runtime settings file

---
+++ src/cp/RuntimeSettings.scala
@@ -0,0 +1,15 @@
+package cp
+
+@serializable class RuntimeSettings {
+  var experimental : Boolean = purescala.Settings.experimental 
+  var showIDs: Boolean = purescala.Settings.showIDs
+  var noForallAxioms: Boolean = purescala.Settings.noForallAxioms
+  var unrollingLevel: Int = purescala.Settings.unrollingLevel
+  var zeroInlining : Boolean = purescala.Settings.zeroInlining 
+  var useBAPA: Boolean = purescala.Settings.useBAPA
+  var useInstantiator: Boolean = purescala.Settings.useInstantiator
+  var useFairInstantiator: Boolean = purescala.Settings.useFairInstantiator
+  var useCores : Boolean = purescala.Settings.useCores 
+  var pruneBranches : Boolean = purescala.Settings.pruneBranches 
+  var solverTimeout : Option[Int] = purescala.Settings.solverTimeout 
+}
