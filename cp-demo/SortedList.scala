26d4f1dece67c4ac6f9da37eacb128cb9e8ae157
Ali Sinan Köksal <alisinan@gmail.com>
2011-05-10 16:26:04 +0000

    version of example that leads to a crash

--- cp-demo/SortedList.scala
+++ cp-demo/SortedList.scala
@@ -11,18 +11,18 @@ import cp.Terms._
         case Cons(_, t) => 1 + size(t)
     }) ensuring(res => res >= 0)
 
-    def isSorted(l: List) : Boolean = l match {
-      case Nil() => true
-      case Cons(x, Nil()) => true
-      case Cons(x, Cons(y, ys)) => x <= y && isSorted(Cons(y, ys))
-    }
-    // def isSorted(l : List) : Boolean = l match {
+    // def isSorted(l: List) : Boolean = l match {
     //   case Nil() => true
-    //   case Cons(x, xs) => xs match {
-    //     case Nil() => true
-    //     case Cons(y, _) => x <= y && isSorted(xs)
-    //   }
+    //   case Cons(x, Nil()) => true
+    //   case Cons(x, Cons(y, ys)) => x <= y && isSorted(Cons(y, ys))
     // }
+    def isSorted(l : List) : Boolean = l match {
+      case Nil() => true
+      case Cons(x, xs) => xs match {
+        case Nil() => true
+        case Cons(y, _) => x <= y && isSorted(xs)
+      }
+    }
 
     def valuesWithin(l: List, lower: Int, upper: Int) : Boolean = l match {
       case Nil() => true
@@ -37,14 +37,7 @@ object SortedList {
     val len = if (args.isEmpty) 3 else args(0).toInt
     val set = scala.collection.mutable.Set[List]()
 
-    val c1 : Constraint1[List] = (l : List) => isSorted(l)
-    val c2 : Constraint1[List] = (l : List) => valuesWithin(l, 0, len)
-    val c3 : Constraint1[List] = (l : List) => size(l) == len
-
-    val combined = c1 && c2 && c3
-
-    // for (list <- ((l : List) => isSorted(l) && valuesWithin(l, 0, len) && size(l) == len).findAll)
-    for (list <- combined.findAll)
+    for (list <- ((l : List) => isSorted(l) && valuesWithin(l, 0, len) && size(l) == len).findAll)
       set += list
       
     println("size : " + set.size)
