6199a72285325f30733874cefa76f6e758bf8f62
Ali Sinan Köksal <alisinan@gmail.com>
2011-07-28 21:35:03 +0000

    calendar example with Scala 2.9

--- cp-demo/executing-specs/Calendar.scala
+++ cp-demo/executing-specs/Calendar.scala
@@ -1,9 +1,18 @@
 import cp.Definitions._
 import cp.Terms._
 
-object Calendar extends Application {
+object Calendar extends App {
   
-  /* maybe more readable with leapDaysUntil's
+  final val totalDays = 10593
+  final val originYear = 1980
+
+  val (year, day) = ((year : Int, day : Int) => totalDays == (year - originYear) * 365 + (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400 -
+    ((originYear - 1) / 4 - (originYear - 1) / 100 + (originYear - 1) / 400) + day &&
+    day > 0 && day <= 366).solve
+
+  println("Year : %d, day : %d" format (year, day))
+
+  /* alternate version with intermediate "leapDaysUntil" variables
 
   def yearAndDay(totalDays : Int, originYear : Int) : (Int, Int) = {
     val (y, d, _, _) = ((year : Int, day : Int, lduYear : Int, lduOrigin : Int) => totalDays == (year - originYear) * 365 + lduYear - lduOrigin + day &&
@@ -13,17 +22,7 @@ object Calendar extends Application {
     
     (y, d)
   }
-
-  println(yearAndDay(10593, 1980))
   */
     
-  final val totalDays = 10593
-  final val originYear = 1980
-
-  val (year, day) = ((year : Int, day : Int) => totalDays == (year - originYear) * 365 + (year - 1) / 4 - (year - 1) / 100 + (year - 1) / 400 -
-    ((originYear - 1) / 4 - (originYear - 1) / 100 + (originYear - 1) / 400) + day &&
-    day > 0 && day <= 366).solve
-
-  println("Year : %d, day : %d" format (year, day))
 }
 
