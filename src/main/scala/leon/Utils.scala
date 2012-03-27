45ccabe12537c0964656384727e4bd5ce18b82dc
Régis Blanc <regwblanc@gmail.com>
2012-03-27 14:36:46 +0000

    Construct to write invariant

--- src/main/scala/leon/Utils.scala
+++ src/main/scala/leon/Utils.scala
@@ -9,4 +9,10 @@ object Utils {
   }
 
   implicit def any2IsValid(x: Boolean) : IsValid = new IsValid(x)
+
+
+  object InvariantFunction {
+    def invariant(x: Boolean): Unit = ()
+  }
+  implicit def while2Invariant(u: Unit) = InvariantFunction
 }
