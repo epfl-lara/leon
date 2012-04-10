aee50880b52302b7def17e10e7fb78511c2b58b5
Régis Blanc <regwblanc@gmail.com>
2012-04-10 17:19:12 +0200

    some fix to the functional bublle sort, now compiling

--- mytest/BubbleFun.scala
+++ mytest/BubbleFun.scala
@@ -1,24 +1,24 @@
-object Bubble[29] {
+object BubbleFun {
 
 
     def sort(a: Map[Int,Int], size: Int): Map[Int,Int] = {
       val i = size - 1
       val t = sortWhile(0, a, i, a, i, size, a)
-      t[94]._2
+      t._2
     }
 
     def sortWhile(
-      j: Int, sortedArray: Map[Int,Int], i: Int, a: Map[Int,Int], i: Int, 
+      j: Int, sortedArray: Map[Int,Int], i: Int, a: Map[Int,Int], i2: Int, 
       size: Int, sortedArray2 : Map[Int,Int]) : (Int, Map[Int,Int], Int) = 
         if ((i > 0)) {
-          val t = sortNestedWhile(sortedArray, 0, sortedArray, i, size, i, sortedArray2, a)
-          sortWhile(t._2, t._1, i - 1, a, i, size, sortedArray2))
+          val t = sortNestedWhile(sortedArray, 0, sortedArray, i, size, i2, sortedArray2, a)
+          sortWhile(t._2, t._1, i - 1, a, i2, size, sortedArray2)
         } else (j, sortedArray, i)
 
 
     def sortNestedWhile(
       sortedArray: Map[Int,Int], j: Int, sortedArray2 : Map[Int,Int], i: Int, 
-      size : Int, i2 : Int, sortedArray3 : Map[Int,Int], a] : Map[Int,Int]) : (Map[Int,Int], Int) =
+      size: Int, i2: Int, sortedArray3: Map[Int,Int], a: Map[Int,Int]) : (Map[Int,Int], Int) =
         if(j < i) {
           val newSortedArray = 
             if(sortedArray(j) > sortedArray(j + 1))
@@ -50,7 +50,7 @@ object Bubble[29] {
         isArrayRec(0, size, a)
 
     def isArrayRec(i: Int, size: Int, a: Map[Int,Int]): Boolean = 
-      if ((i[27] >= size))
+      if (i >= size)
         true
       else {
         if (a.isDefinedAt(i))
@@ -62,24 +62,24 @@ object Bubble[29] {
 
     def partitioned(a: Map[Int,Int], size: Int, l1: Int, u1: Int, l2: Int, u2: Int) : Boolean = {
       val t = partitionedWhile(l2, true, l1, l1, size, u2, l2, l2, u1, l1, a)
-      t[56]._2
+      t._2
     }
 
     def partitionedWhile(
       j: Int, isPartitionned: Boolean, i: Int, l1: Int, size: Int, u2: Int, 
-      l2: Int, j : Int, u1: Int, i2 : Int, a: Map[Int,Int]) : (Int, Boolean, Int) =
+      l2: Int, j2: Int, u1: Int, i2 : Int, a: Map[Int,Int]) : (Int, Boolean, Int) =
         if((i <= u1)) {
           val t = partitionedNestedWhile(
-                    isPartitionned, l2, l1, u1, size, j, u2, a, l2, l2, i2, i
+                    isPartitionned, l2, l1, u1, size, j2, u2, a, l2, l2, i2, i
                   )
-          partitionedWhile(t._2, t._1, i + 1, l1, size, u2, l2, j, u1, i2, a)
+          partitionedWhile(t._2, t._1, i + 1, l1, size, u2, l2, j2, u1, i2, a)
         } else (j, isPartitionned, i)
 
-    def partitionedNestedwhile(
+    def partitionedNestedWhile(
       isPartitionned: Boolean, j: Int, l1: Int, u1: Int, size: Int, j2: Int, u2: Int, 
       a: Map[Int,Int], j3 : Int, l2: Int, i2: Int, i: Int) : (Boolean, Int) =
         if (j <= u2) {
-          partitionedNestedwhile(
+          partitionedNestedWhile(
             (if ((a(i) > a(j)))
               false
             else
