f810bd7a88051ab295a3cf76027f0be527edfb2f
Régis Blanc <regwblanc@gmail.com>
2012-04-20 18:11:02 +0200

    removed println

--- src/main/scala/leon/Main.scala
+++ src/main/scala/leon/Main.scala
@@ -31,7 +31,6 @@ object Main {
   }
 
   private def defaultAction(program: Program, reporter: Reporter) : Unit = {
-    println("Input program is:\n" + program)
     val passManager = new PassManager(Seq(ImperativeCodeElimination, UnitElimination, FunctionClosure, FunctionHoisting, Simplificator))
     val program2 = passManager.run(program)
     val analysis = new Analysis(program2, reporter)
