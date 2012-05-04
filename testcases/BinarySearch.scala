2b898e919710a84927827665a61feadd3ad19634
Régis Blanc <regwblanc@gmail.com>
2012-05-04 15:14:18 +0200

    buggy binary search

--- testcases/BinarySearch.scala
+++ testcases/BinarySearch.scala
@@ -4,10 +4,10 @@ import leon.Utils._
 
 object BinarySearch {
 
-  def binarySearch(a: Map[Int, Int], size: Int, key: Int): Int = ({
-    require(isArray(a, size) && size < 5 && sorted(a, size, 0, size - 1))
+  def binarySearch(a: Array[Int], key: Int): Int = ({
+    require(a.length > 0 && sorted(a, 0, a.length - 1))
     var low = 0
-    var high = size - 1
+    var high = a.length - 1
     var res = -1
 
     (while(low <= high && res == -1) {
@@ -21,13 +21,24 @@ object BinarySearch {
         high = i - 1
       else if(v < key)
         low = i + 1
-    }) invariant(0 <= low && low <= high + 1 && high < size && (if(res >= 0) a(res) == key else (!occurs(a, 0, low, key) && !occurs(a, high + 1, size, key))))
+    }) invariant(
+        0 <= low && 
+        low <= high + 1 && 
+        high < a.length && 
+        (if (res >= 0) 
+            a(res) == key else 
+            (!occurs(a, 0, low, key) && !occurs(a, high + 1, a.length, key))
+        )
+       )
     res
-  }) ensuring(res => res >= -1 && res < size && (if(res >= 0) a(res) == key else !occurs(a, 0, size, key)))
+  }) ensuring(res => 
+      res >= -1 && 
+      res < a.length && 
+      (if(res >= 0) a(res) == key else !occurs(a, 0, a.length, key)))
 
 
-  def occurs(a: Map[Int, Int], from: Int, to: Int, key: Int): Boolean = {
-    require(isArray(a, to) && to < 5 && from >= 0)
+  def occurs(a: Array[Int], from: Int, to: Int, key: Int): Boolean = {
+    require(a.length >= 0 && to <= a.length && from >= 0)
     def rec(i: Int): Boolean = {
       require(i >= 0)
       if(i >= to) 
@@ -43,31 +54,16 @@ object BinarySearch {
   }
 
 
-  def isArray(a: Map[Int, Int], size: Int): Boolean = {
-
-    def rec(i: Int): Boolean = {
-      require(i >= 0)  
-      if(i >= size) true else {
-        if(a.isDefinedAt(i)) rec(i+1) else false
-      }
-    }
-
-    if(size < 0)
-      false
-    else
-      rec(0)
-  }
-
-  def sorted(a: Map[Int,Int], size: Int, l: Int, u: Int) : Boolean = {
-    require(isArray(a, size) && size < 5 && l >= 0 && l <= u && u < size)
-    val t = sortedWhile(true, l, l, u, a, size)
+  def sorted(a: Array[Int], l: Int, u: Int) : Boolean = {
+    require(a.length >= 0 && l >= 0 && l <= u && u < a.length)
+    val t = sortedWhile(true, l, l, u, a)
     t._1
   }
 
-  def sortedWhile(isSorted: Boolean, k: Int, l: Int, u: Int, a: Map[Int,Int], size: Int) : (Boolean, Int) = {
-    require(isArray(a, size) && size < 5 && l >= 0 && l <= u && u < size && k >= l && k <= u)
+  def sortedWhile(isSorted: Boolean, k: Int, l: Int, u: Int, a: Array[Int]): (Boolean, Int) = {
+    require(a.length >= 0 && l >= 0 && l <= u && u < a.length && k >= l && k <= u)
     if(k < u) {
-      sortedWhile(if(a(k) > a(k + 1)) false else isSorted, k + 1, l, u, a, size)
+      sortedWhile(if(a(k) > a(k + 1)) false else isSorted, k + 1, l, u, a)
     } else (isSorted, k)
   }
 }
