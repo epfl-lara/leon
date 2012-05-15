e4b04d27efc2bcf32d8917a7cfaef3b46d90cbcf
Régis Blanc <regwblanc@gmail.com>
2012-05-15 17:17:03 +0200

    hoisting ite function

--- src/main/scala/leon/CallGraph.scala
+++ src/main/scala/leon/CallGraph.scala
@@ -26,11 +26,86 @@ class CallGraph(val program: Program) {
     null
   }
 
-//  def hoistIte(expr: Expr): Expr = expr match {
-//    case ite@IfExpr(c, t, e) => IfExpr(c, hoistIte(t), hoistIte(e)).setType(ite.getType)
-//    case BinaryOperator(t1, t2) =>
-//
-//  }
+  def hoistIte(expr: Expr): Expr = {
+
+    def transform(expr: Expr): Expr = expr match {
+      case ite@IfExpr(c, t, e) => ite
+      case BinaryOperator(IfExpr(c, t, e), t2, op) => IfExpr(c, op(t, t2), op(e, t2))
+      case BinaryOperator(t1, IfExpr(c, t, e), op) => IfExpr(c, op(t1, t), op(t1, e))
+      case (t: Terminal) => t
+    }
+
+    def fix[A](f: (A) => A, a: A): A = {
+      val na = f(a)
+      if(a == na) a else fix(f, na)
+    }
+    fix(transform, expr)
+  }
+
+  //def hoistIte(expr: Expr): (Seq[Expr] => Expr, Seq[Expr]) = expr match { 
+  //  case ite@IfExpr(c, t, e) => {
+  //    val (iteThen, valsThen) = hoistIte(t)
+  //    val nbValsThen = valsThen.size
+  //    val (iteElse, valsElse) = hoistIte(e)
+  //    val nbValsElse = valsElse.size
+  //    def ite(es: Seq[Expr]): Expr = {
+  //      val argsThen = es.take(nbValsThen)
+  //      val argsElse = es.drop(nbValsThen)
+  //      IfExpr(c, iteThen(argsThen), iteElse(argsElse), e2)
+  //    }
+  //    (ite, valsThen ++ valsElse)
+  //  }
+  //  case BinaryOperator(t1, t2, op) => {
+  //    val (iteLeft, valsLeft) = hoistIte(t1)
+  //    val (iteRight, valsRight) = hoistIte(t2)
+  //    def ite(e1: Expr, e2: Expr): Expr = {
+
+  //    }
+  //    iteLeft(
+  //      iteRight(
+  //        op(thenValRight, thenValLeft),
+  //        op(thenValRight, elseValLeft)
+  //      ), iteRight(
+  //        op(elseValRight, thenValLeft),
+  //        op(elseValRight, elseValLeft)
+  //      )
+  //    )
+  //  }
+  //  case NAryOperator(args, op) => {
+
+  //  }
+  //  case (t: Terminal) => {
+  //    def ite(es: Seq[Expr]): Expr = {
+  //      require(es.size == 1)
+  //      es.head
+  //    }
+  //    (ite, Seq(t))
+  //  }
+  //  case _ => scala.sys.error("Unhandled tree in hoistIte : " + expr)
+  //}
+
+  // prec: there should be no lets and no pattern-matching in this expression
+  //private def collectWithPathCondition(expression: Expr, cond: Seq[Expr]): Seq[Expr] = {
+  //  var allPaths: Seq[Expr] = Seq()
+
+  //  def rec(expr: Expr, path: Seq[Expr]): Seq[Expr] = {
+  //    expr match {
+  //      case FunctionInvocLabel(fd, args) =>
+  //      case WayPoint(e) =>
+  //    }
+  //    
+  //    case IfExpr(cond, then, elze) =>
+  //      (if(!containsIfExpr(then)) Seq(And(path, cond)) else rec(then, And(cond, path))) ++
+  //      (if(!containsIfExpr(elze)) Seq(And(path, Not(cond))) else rec(then, And(cond, path)))
+  //    case NAryOperator(args, _) => args.flatMap(arg => rec(arg, path))
+  //    case BinaryOperator(t1, t2, _) => rec(t1, path) ++ rec(t2, path)
+  //    case UnaryOperator(t, _) => rec(t, path)
+  //    case t : Terminal => Seq()
+  //    case _ => scala.sys.error("Unhandled tree in collectWithPathCondition : " + expr)
+  //  }
+
+  //  rec(expression, BooleanLiteral(true)).distinct
+  //}
 
   //def analyse(program: Program) {
   //  z3Solver.setProgram(program)
@@ -49,21 +124,4 @@ class CallGraph(val program: Program) {
   //  collectWithPathCondition(cleanBody)
   //}
 
-  //// prec: there should be no lets and no pattern-matching in this expression
-  //  private def collectWithPathCondition(expression: Expr): Seq[Expr] = {
-  //  var allPaths: Seq[Expr] = Seq()
-
-  //  def rec(expr: Expr, path: Expr): Seq[Expr] = expr match {
-  //    case IfExpr(cond, then, elze) =>
-  //      (if(!containsIfExpr(then)) Seq(And(path, cond)) else rec(then, And(cond, path))) ++
-  //      (if(!containsIfExpr(elze)) Seq(And(path, Not(cond))) else rec(then, And(cond, path)))
-  //    case NAryOperator(args, _) => args.flatMap(arg => rec(arg, path))
-  //    case BinaryOperator(t1, t2, _) => rec(t1, path) ++ rec(t2, path)
-  //    case UnaryOperator(t, _) => rec(t, path)
-  //    case t : Terminal => Seq()
-  //    case _ => scala.sys.error("Unhandled tree in collectWithPathCondition : " + expr)
-  //  }
-
-  //  rec(expression, BooleanLiteral(true)).distinct
-  //}
 }
