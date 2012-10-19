8c389bd4508ccb402b98e66934cf5b86990af43d
Etienne Kneuss <ekneuss@gmail.com>
2012-10-19 16:54:11 +0200

    Add choose up to 7 arguments

--- library/Utils.scala
+++ library/Utils.scala
@@ -19,4 +19,14 @@ object Utils {
 
 
   def waypoint[A](i: Int, expr: A): A = expr
+
+  private def noChoose = throw new RuntimeException("Implementation not supported")
+
+  def choose[A](predicate: A => Boolean) = noChoose
+  def choose[A, B](predicate: (A, B) => Boolean) = noChoose
+  def choose[A, B, C](predicate: (A, B, C) => Boolean) = noChoose
+  def choose[A, B, C, D](predicate: (A, B, C, D) => Boolean) = noChoose
+  def choose[A, B, C, D, E](predicate: (A, B, C, D, E) => Boolean) = noChoose
+  def choose[A, B, C, D, E, F](predicate: (A, B, C, D, E, F) => Boolean) = noChoose
+  def choose[A, B, C, D, E, F, G](predicate: (A, B, C, D, E, F, G) => Boolean) = noChoose
 }
