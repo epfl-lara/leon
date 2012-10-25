c00ba9cdd4e769ac0fa6d534c64f21e9729ef610
Philippe Suter <philippe.suter@gmail.com>
2012-10-25 17:27:56 +0200

    Etienne is interrupting

--- src/test/scala/leon/test/ValidPrograms.scala
+++ src/test/scala/leon/test/ValidPrograms.scala
@@ -3,8 +3,15 @@ package test
 
 import org.scalatest.FunSuite
 
+import java.io.File
+
 class ValidPrograms extends FunSuite {
-  test("Some program is valid.") {
+  def runBasicLeonOnFile(filename : String) : Unit = {
+    val file = new File(filename)
+
+    assert(file.exists && file.isFile && file.canRead,
+           "Benchmark [%s] is not a readable file".format(filename))
+
     val ctx = LeonContext(
       Settings(
         synthesis = false,
@@ -14,10 +21,26 @@ class ValidPrograms extends FunSuite {
       new SilentReporter
     )
 
-    val pipeline = Main.computePipeline(ctx.settings) 
-
-    val result = pipeline.run(ctx)("/home/psuter/Test.scala" :: Nil)
-
-    assert(true)
+    val pipeline = Main.computePipeline(ctx.settings)
+    pipeline.run(ctx)("--timeout=2" :: file.getPath :: Nil)
   }
+
+  def mkTest(filename : String) = {
+    test("Valid PureScala program: [%s]".format(filename)) {
+      runBasicLeonOnFile(filename)
+      assert(true)
+    }
+  }
+
+  test("List files") {
+    import scala.collection.JavaConversions._
+
+    val ress = this.getClass.getClassLoader.getResources("/regression/verification/purescala/valid/")
+
+    for(res <- ress) {
+      println(res)
+    }
+  }
+
+  mkTest("/home/psuter/Test.scala")
 }
