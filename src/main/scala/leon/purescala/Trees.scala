ebadfbce8837a4762045c6868de88fefe825a724
Etienne Kneuss <ekneuss@gmail.com>
2012-10-26 19:26:49 +0200

    Make sure position is set

--- src/main/scala/leon/purescala/Trees.scala
+++ src/main/scala/leon/purescala/Trees.scala
@@ -44,7 +44,7 @@ object Trees {
   case class Epsilon(pred: Expr) extends Expr with ScalacPositional
 
   case class Choose(vars: List[Identifier], pred: Expr) extends Expr with ScalacPositional with UnaryExtractable {
-    def extract = Some((pred, (e: Expr) => Choose(vars, e)))
+    def extract = Some((pred, (e: Expr) => Choose(vars, e).setPosInfo(this)))
   }
 
   /* Like vals */
