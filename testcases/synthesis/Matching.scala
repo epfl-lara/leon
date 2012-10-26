eba480cabc9ae5807c4cfede64e06583bce77b27
Etienne Kneuss <ekneuss@gmail.com>
2012-10-26 17:42:47 +0200

    Add testcase for matching reconstruction

---
+++ testcases/synthesis/Matching.scala
@@ -0,0 +1,13 @@
+import leon.Utils._
+
+object Matching {
+    def t1(a: NatList) = choose( (x: Nat) => Cons(x, Nil()) == a)
+
+  abstract class Nat
+  case class Z() extends Nat
+  case class Succ(n: Nat) extends Nat
+
+  abstract class NatList
+  case class Nil() extends NatList
+  case class Cons(head: Nat, tail: NatList) extends NatList
+}
