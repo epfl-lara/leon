b5625a2c99fe763fab8051a681310dff21307655
Philippe Suter <philippe.suter@gmail.com>
2012-11-01 16:51:03 +0100

    one small example

---
+++ testcases/synthesis/DrSuter.scala
@@ -0,0 +1,17 @@
+import leon.Utils._
+
+object DrSuter {
+
+  abstract class List
+  case class Nil() extends List
+  case class Cons(head : Int, tail : List) extends List
+
+  
+
+  def myChoose(a : List) : (Int,Int,List) = {
+    choose { (x: Int, y: Int, ys: List) => 
+      Cons(x, Cons(y, ys)) == a && x == y ||
+      Cons(x, ys) == a          && x < 0
+    }
+  }
+}
