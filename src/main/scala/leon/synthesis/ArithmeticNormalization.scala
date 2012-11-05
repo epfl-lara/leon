0490335aad0c8c6a1e63adefd9b8dd802b25720c
Régis Blanc <regwblanc@gmail.com>
2012-11-05 19:23:22 +0100

    some arithmetic manipulations with tests

---
+++ src/main/scala/leon/synthesis/ArithmeticNormalization.scala
@@ -0,0 +1,76 @@
+package leon.synthesis
+
+import leon.purescala.Trees._
+import leon.purescala.TreeOps._
+import leon.purescala.Common._
+
+object ArithmeticNormalization {
+
+  //assume the function is an arithmetic expression, not a relation
+  //return a normal form where the [t a1 ... an] where
+  //expr = t + a1*x1 + ... + an*xn and xs = [x1 ... xn]
+  def apply(expr: Expr, xs: Array[Identifier]): Array[Expr] = {
+
+    def containsId(e: Expr, id: Identifier): Boolean = e match {
+      case Times(e1, e2) => containsId(e1, id) || containsId(e2, id)
+      case IntLiteral(_) => false
+      case Variable(id2) => id == id2
+      case _ => sys.error("unexpected format")
+    }
+
+    def group(es: Seq[Expr], id: Identifier): Expr = {
+      val totalCoef = es.foldLeft(0)((acc, e) => {
+        val (IntLiteral(i), id2) = extractCoef(e)
+        assert(id2 == id)
+        acc + i
+      })
+      Times(IntLiteral(totalCoef), Variable(id))
+    }
+
+    var expandedForm: Seq[Expr] = expand(expr)
+    val res: Array[Expr] = new Array(xs.size + 1)
+
+    xs.zipWithIndex.foreach{case (id, index) => {
+      val (terms, rests) = expandedForm.partition(containsId(_, id))
+      expandedForm = rests
+      res(index+1) = group(terms, id)
+    }}
+
+    res(0) = expandedForm.foldLeft[Expr](IntLiteral(0))(Plus(_, _))
+    res
+  }
+
+
+  //assume the expr is a literal (mult of constants and variables) with degree one
+  def extractCoef(e: Expr): (Expr, Identifier) = {
+    var id: Option[Identifier] = None
+    var coef = 1
+
+    def rec(e: Expr): Unit = e match {
+      case IntLiteral(i) => coef = coef*i
+      case Variable(id2) => if(id.isEmpty) id = Some(id2) else sys.error("multiple variables")
+      case Times(e1, e2) => rec(e1); rec(e2)
+    }
+
+    rec(e)
+    assert(!id.isEmpty)
+    (IntLiteral(coef), id.get)
+  }
+
+  //multiply two sums together and distribute in a bigger sum
+  def multiply(es1: Seq[Expr], es2: Seq[Expr]): Seq[Expr] = {
+    es1.flatMap(e1 => es2.map(e2 => Times(e1, e2)))
+  }
+
+
+  def expand(expr: Expr): Seq[Expr] = expr match {
+    case Plus(es1, es2) => expand(es1) ++ expand(es2)
+    case Minus(e1, e2) => Times(IntLiteral(-1), e2) +: expand(e1)
+    case Times(es1, es2) => multiply(expand(es1), expand(es2))
+    case v@Variable(_) => Seq(v)
+    case n@IntLiteral(_) => Seq(n)
+    case _ => sys.error("Unexpected")
+  }
+
+
+}
---
+++ src/test/scala/leon/test/synthesis/ArithmeticNormalizationSuite.scala
@@ -0,0 +1,67 @@
+package leon.test.synthesis
+
+import org.scalatest.FunSuite
+
+import leon.Evaluator
+import leon.purescala.Trees._
+import leon.purescala.Common._
+
+import leon.synthesis.ArithmeticNormalization._
+
+class ArithmeticNormalizationSuite extends FunSuite {
+
+  def i(x: Int) = IntLiteral(x)
+
+  val xId = FreshIdentifier("x")
+  val x = Variable(xId)
+  val yId = FreshIdentifier("y")
+  val y = Variable(yId)
+  
+  val allMaps: Seq[Map[Identifier, Expr]] = (-20 to 20).flatMap(xVal => (-20 to 20).map(yVal => Map(xId-> i(xVal), yId -> i(yVal))))
+
+  def checkSameExpr(e1: Expr, e2: Expr) {
+    allMaps.foreach(m =>
+      assert(Evaluator.eval(m, e1, None) === Evaluator.eval(m, e2, None))
+    )
+  }
+
+  def toSum(es: Seq[Expr]) = es.reduceLeft(Plus(_, _))
+  
+  test("checkSameExpr") {
+    checkSameExpr(Plus(x, y), Plus(y, x))
+    checkSameExpr(Plus(x, x), Times(x, i(2)))
+    checkSameExpr(Plus(x, Plus(x, x)), Times(x, i(3)))
+  }
+
+  test("multiply") {
+    val lhs = Seq(x, i(2))
+    val rhs = Seq(y, i(1))
+    checkSameExpr(Times(toSum(lhs), toSum(rhs)), toSum(multiply(lhs, rhs)))
+    checkSameExpr(Times(toSum(rhs), toSum(lhs)), toSum(multiply(rhs, lhs)))
+
+    val lhs2 = Seq(x, y, i(2))
+    val rhs2 = Seq(y, i(1), Times(i(2), x))
+    checkSameExpr(Times(toSum(lhs2), toSum(rhs2)), toSum(multiply(lhs2, rhs2)))
+  }
+
+  test("expand") {
+    val e1 = Times(Plus(x, i(2)), Plus(y, i(1)))
+    checkSameExpr(toSum(expand(e1)), e1)
+
+    val e2 = Times(Plus(x, Times(i(2), y)), Plus(Plus(x, y), i(1)))
+    checkSameExpr(toSum(expand(e2)), e2)
+
+  }
+
+  test("apply") {
+    val xs = Array(xId, yId)
+
+    val e1 = Plus(Times(Plus(x, i(2)), i(3)), Times(i(4), y))
+    println(apply(e1, xs).mkString(" + "))
+    checkSameExpr(toSum(apply(e1, xs)), e1)
+
+
+  }
+  
+
+}
