1bd1e39846a696cb3a3cdae9c26ee97e1ad7a3ec
Régis Blanc <regwblanc@gmail.com>
2012-11-13 08:45:16 +0000

    integer synthesis

--- src/main/scala/leon/synthesis/IntegerSynthesis.scala
+++ src/main/scala/leon/synthesis/IntegerSynthesis.scala
@@ -8,26 +8,31 @@ import LinearEquations._
 
 object IntegerSynthesis {
 
-  def apply(as: Set[Identifier], xs: Set[Identifier], formula: Expr): (Expr, List[Expr]) = {
-    extractEquals(formula) match {
-      case (Some(eq), f) => {
-        val vars: Set[Identifier] = variablesOf(eq)
-        val eqxs: List[Identifier] = xs.intersect(vars).toList
-        println("eqxs: " + eqxs)
-        val eqas: Set[Identifier] = as.intersect(vars)
-        println("formula: " + formula)
-        println("eq: " + eq)
-        val normalizedEq: List[Expr] = ArithmeticNormalization(Minus(eq.left, eq.right), eqxs.toArray).toList
-        println("normalized eq: " + normalizedEq)
-        val (pre, subst, freshVars) = elimVariable(eqas, normalizedEq)
-        println("subst: " + subst.mkString("\n"))
-        val substMap: Map[Expr, Expr] = eqxs.zip(subst).map{case (id, e) => (Variable(id), e)}.toMap
-        val freshFormula = simplify(replace(substMap, f))
-        println("fresh formula is: " + freshFormula)
-        null
-      }
-      case (None, f) => null
-    }
+  def apply(as: Set[Identifier], xs: List[Identifier], formula: Expr): (Expr, List[Expr]) = {
+    null
+    //extractEquals(formula) match {
+    //  case (None, f) => (f, xs.map(id => (Variable(id))))
+    //  case (Some(eq), f) => {
+    //    val vars: Set[Identifier] = variablesOf(eq)
+    //    val eqxs: List[Identifier] = xs.toSet.intersect(vars).toList
+    //    val ys: Set[Identifier] = xs.toSet.difference(vars).toList
+    //    val eqas: Set[Identifier] = as.intersect(vars)
+    //    val normalizedEq: List[Expr] = ArithmeticNormalization(Minus(eq.left, eq.right), eqxs.toArray).toList
+    //    val (eqPre, eqWitness, eqFreshVars) = elimVariable(eqas, normalizedEq)
+    //    val eqSubstMap: Map[Expr, Expr] = eqxs.zip(eqWitness).map{case (id, e) => (Variable(id), e)}.toMap
+    //    val freshFormula = simplify(replace(eqSubstMap, f))
+    //    val (recPre, recSubst) = apply(as, ys ++ eqFreshVars, freshFormula)
+
+    //    val freshPre = simplify(replace(
+    //      (ys++freshVars).zip(recSubst).map{
+    //        case (id, e) => (Variable(id), e)
+    //      }.toMap,
+    //      eqPre))
+
+    //    (And(freshPre, recPre), 
+
+    //  }
+    //}
   }
 
 }
