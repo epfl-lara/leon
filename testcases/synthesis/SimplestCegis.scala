6d78ea595a63b8b347a49d9fafab125845230f70
Etienne Kneuss <ekneuss@gmail.com>
2012-11-13 17:08:01 +0100

    Add some testcases

---
+++ testcases/synthesis/SimplestCegis.scala
@@ -0,0 +1,17 @@
+import scala.collection.immutable.Set
+import leon.Annotations._
+import leon.Utils._
+
+object Injection {
+  sealed abstract class List
+  case class Cons(tail: List) extends List
+  case class Nil() extends List
+
+  // proved with unrolling=0
+  def size(l: List) : Int = (l match {
+      case Nil() => 0
+      case Cons(t) => 1 + size(t)
+  }) ensuring(res => res >= 0)
+
+  def simple(in: List) = choose{out: List => size(out) == size(in) }
+}
