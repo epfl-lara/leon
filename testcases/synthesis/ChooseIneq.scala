54cd8c4db9ba04ad14bc44625e3904a286f18adb
Philippe Suter <philippe.suter@gmail.com>
2012-11-16 18:27:58 +0100

    one more example for integers

--- testcases/synthesis/ChooseIneq.scala
+++ testcases/synthesis/ChooseIneq.scala
@@ -4,4 +4,8 @@ object ChooseIneq {
   def c1(a: Int, b: Int): (Int, Int) = { 
     choose ((x: Int, y: Int) => -3*x + 2*y -b <= a && 2*x - a <= 4*y + b)
   }
+
+  def c2(a : Int) : (Int, Int) = choose {
+    (x : Int, y : Int) => 5*x + 7*y == a && 0 <= x && x <= y
+  }
 }
