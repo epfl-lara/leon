b00a324ca6eb34868f3ba715babd12a4f2d83c05
Régis Blanc <regwblanc@gmail.com>
2012-11-16 15:52:11 +0100

    sec 2 time

---
+++ testcases/synthesis/Sec2Time.scala
@@ -0,0 +1,7 @@
+import leon.Utils._
+
+object Sec2Time {
+
+  def s2t(total: Int) : (Int, Int, Int) = choose((h: Int, m: Int, s: Int) => 3600*h + 60*m + s == total && h >= 0 && m >= 0 && m < 60 && s >= 0 && s < 60)
+
+}
