6fdae6185abcf97749e950210de26278017ac51c
Philippe Suter <philippe.suter@gmail.com>
2012-12-04 02:23:14 +0100

    Progress in CodeGen.
    - Recursive functions that manipulate only integers.
    - "Smart conditionals" compilation scheme.

--- src/test/resources/regression/codegen/purescala/Prog001.scala
+++ src/test/resources/regression/codegen/purescala/Prog001.scala
@@ -1,5 +1,18 @@
 object Prog001 {
   def fortyTwo() = 42
 
-  // def plus(x : Int, y : Int) = x + y
+  def plus(x : Int, y : Int) = x + y
+
+  def double(x : Int) : Int = {
+    val a = x
+    a + a
+  }
+
+  // def implies(a : Boolean, b : Boolean) : Boolean = !a || b
+
+  def abs(x : Int) : Int = {
+    if(x < 0) -x else x
+  }
+
+  def factorial(i : Int) : Int = if(i <= 1) 1 else (i * factorial(i - 1))
 }
