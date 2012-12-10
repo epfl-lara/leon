984286c482ac935f3af1f7b43c1541cf582eb5a6
Philippe Suter <philippe.suter@gmail.com>
2012-12-10 16:41:56 +0100

    Steps towards boxed/unboxed values in CodeGen.

--- src/main/java/leon/codegen/runtime/Tuple.java
+++ src/main/java/leon/codegen/runtime/Tuple.java
@@ -6,6 +6,9 @@ public final class Tuple {
   private int arity;
   private Object[] elements;
 
+  // You may think that using varargs here would show less of the internals,
+  // however the bytecode to generate is exactly the same, so let's reflect
+  // the reality instead.
   public Tuple(int arity, Object[] elements) {
     this.arity = arity;
     this.elements = Arrays.copyOf(elements, elements.length);
