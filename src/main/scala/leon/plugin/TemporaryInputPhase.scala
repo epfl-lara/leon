b1d245771b1d7d580fc5195ce07307c9f4e30b2f
Etienne Kneuss <ekneuss@gmail.com>
2012-12-10 19:00:46 +0100

    Add phase to turn literal string into a file content, with path as option

---
+++ src/main/scala/leon/plugin/TemporaryInputPhase.scala
@@ -0,0 +1,22 @@
+package leon
+package plugin
+
+import java.io.{File, BufferedWriter, FileWriter}
+
+object TemporaryInputPhase extends LeonPhase[(String, List[String]), List[String]] {
+
+  val name = "Temporary Input"
+  val description = "Feed the compiler with a temporary input file"
+
+  def run(ctx: LeonContext)(data: (String, List[String])): List[String] = {
+    val (content, opts) = data
+
+    val file : File = File.createTempFile("leon", ".scala")
+    file.deleteOnExit
+    val out = new BufferedWriter(new FileWriter(file))
+    out.write(content)
+    out.close
+
+    file.getAbsolutePath() :: opts
+  }
+}
