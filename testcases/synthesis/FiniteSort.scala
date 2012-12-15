e230f2b087e4c67a5ca2449890bd333f3c633dc8
Régis Blanc <regwblanc@gmail.com>
2012-12-15 15:59:59 +0100

    testcase for finite sort, with 3 elements

---
+++ testcases/synthesis/FiniteSort.scala
@@ -0,0 +1,18 @@
+import leon.Utils._
+
+object FiniteSort {
+
+  def sort3(x1: Int, x2: Int, x3: Int): (Int, Int, Int) = {
+    choose((z1: Int, z2: Int, z3: Int) => 
+      z1 <= z2 && z2 <= z3 && (
+        (z1 == x1 && z2 == x2 && z3 == x3) ||
+        (z1 == x1 && z2 == x3 && z3 == x2) ||
+        (z1 == x2 && z2 == x1 && z3 == x3) ||
+        (z1 == x2 && z2 == x3 && z3 == x1) ||
+        (z1 == x3 && z2 == x1 && z3 == x2) ||
+        (z1 == x3 && z2 == x2 && z3 == x1)
+      )
+    )
+  }
+
+}
