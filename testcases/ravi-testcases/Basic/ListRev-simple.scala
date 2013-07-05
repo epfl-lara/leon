65cf8cb1ef0fad76b5b2f8fbfe4fa79417b28bb3
ravi <ravi.kandhadai@epfl.ch>
2013-07-05 14:11:36 +0530

    adding one more testcase

---
+++ testcases/ravi-testcases/Basic/ListRev-simple.scala
@@ -0,0 +1,19 @@
+import leon.Utils._
+
+object ListWithSize {
+    sealed abstract class List
+    case class Cons(head: Int, tail: List) extends List
+    case class Nil() extends List
+ 
+    // proved with unrolling=0
+    def size(l: List) : Int = (l match { 
+        case Nil() => 0
+        case Cons(_, t) => 1 + size(t)
+    }) ensuring(res => true template((a) => a*res <= 0 ))
+
+    def append(l1 : List, l2 : List) : List = (l1 match {
+      case Nil() => l2
+      case Cons(x,xs) => Cons(x, append(xs, l2))
+
+    }) ensuring(res => size(res) != size(l1) - 1 template((a,b,c) => a*size(l1) + b*size(res) <= 0))
+}
