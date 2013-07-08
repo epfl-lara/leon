e778d009b99751d3037c68e24ea3a3dd61f7900a
ravi <ravi.kandhadai@epfl.ch>
2013-07-08 13:06:07 +0530

    updations

--- testcases/ravi-testcases/Basic/ListRev.scala
+++ testcases/ravi-testcases/Basic/ListRev.scala
@@ -5,13 +5,11 @@ object ListWithSize {
     case class Cons(head: Int, tail: List) extends List
     case class Nil() extends List
  
-    // proved with unrolling=0
     def size(l: List) : Int = (l match { 
         case Nil() => 0
         case Cons(_, t) => 1 + size(t)
-    }) //ensuring(res => res >= 0)
+    }) 
 
-    // proved with unrolling=2
     def reverse(l: List) : List = { 
       reverse0(l, Nil())  
     } ensuring(res => size(res) == size(l))
