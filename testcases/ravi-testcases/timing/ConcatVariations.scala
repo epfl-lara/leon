65b8c55114d3757c6e25e45b8c4de3c50ef5dc0a
Ravi Kandhadai <ravi@Ravis-MacBook-Pro.local>
2013-11-11 01:35:10 +0100

    updations

--- testcases/ravi-testcases/timing/ConcatVariations.scala
+++ testcases/ravi-testcases/timing/ConcatVariations.scala
@@ -15,13 +15,13 @@ object ListOperations {
     if (n == 0) Nil()
     else
       Cons(n, genL(n - 1))
-  } ensuring (res => size(res) == n && time <= 3*n + 2)
+  } ensuring (res => size(res) == n template((a,b) => time <= a*n + b))
 
   def append(l1: List, l2: List): List = (l1 match {
     case Nil() => l2
     case Cons(x, xs) => Cons(x, append(xs, l2))
 
-  }) ensuring (res =>  size(l1) + size(l2) == size(res) && time <= 3*size(l1) + 2)
+  }) ensuring (res =>  size(l1) + size(l2) == size(res) template((a,b) => time <= a*size(l1) + b))
   
   def mult(x : Int, y : Int) : Int = {
       if(x == 0 || y == 0) 0
@@ -29,18 +29,18 @@ object ListOperations {
     	  mult(x,y-1) + x
   } 
 
-//  def f_worst(m: Int, n: Int): List = {
-//    require(0 <= m && 0 <= n)
-//    
-//    if (m == 0) Nil()
-//    else append(f_worst(m - 1, n), genL(n))
-//    
-//  } ensuring(res => size(res) == mult(n, m) template((a,b,c) => time <= a*mult(mult(n,m),m+1) + c))
-
   def f_worst(m: Int, n: Int): List = {
     require(0 <= m && 0 <= n)
-    if (m == 0) Nil()
-    else append(genL(n), f_worst(m - 1, n))
     
-  } ensuring(res => size(res) == mult(n, m) && time <= 5*size(res) + 6)
+    if (m == 0) Nil()
+    else append(f_worst(m - 1, n), genL(n))
+    
+  } ensuring(res => size(res) == mult(n, m) template((a,b,c,d) => time <= a*mult(mult(n,m),m+1) + b*n + c*m + d))
+
+  def f_good(m: Int, n: Int): List = {
+    require(0 <= m && 0 <= n)
+    if (m == 0) Nil()
+    else append(genL(n), f_good(m - 1, n))
+    
+  } ensuring(res => size(res) == mult(n, m) template((a,b,c,d) => time <= a*mult(n,m) + b*n + c*m +d))
 }
