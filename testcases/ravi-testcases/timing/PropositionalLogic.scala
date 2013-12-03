7ea8cfc12b4077507231cc2604462b39106ed5fb
Ravichandhran Kandhadai Madhavan <kandhada@larapc12.epfl.ch>
2013-12-03 12:44:11 +0100

    Adding new results after fixing the bugs

--- testcases/ravi-testcases/timing/PropositionalLogic.scala
+++ testcases/ravi-testcases/timing/PropositionalLogic.scala
@@ -47,7 +47,8 @@ object PropositionalLogic {
     case Not(Not(f)) => nnf(f)
     case Not(Literal(_)) => formula
     case Literal(_) => formula
-  }) //ensuring(isNNF(_) && )
+    case _ => formula
+  }) ensuring((res) => true template((a,b) => time <= a*size(formula) + b))
 
   def isNNF(f: Formula): Boolean = { f match {
     case And(lhs, rhs) => isNNF(lhs) && isNNF(rhs)
