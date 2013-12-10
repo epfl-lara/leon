608c4d204c4e123b1b0f609a7a2ba80407605d1f
Ravichandhran Kandhadai Madhavan <kandhada@larapc12.epfl.ch>
2013-12-10 18:51:34 +0100

    Fixing bugs in maps

--- testcases/ravi-testcases/obspurity/ex1.scala
+++ testcases/ravi-testcases/obspurity/ex1.scala
@@ -1,56 +1,50 @@
 import leon.Utils._
 
 object ObsPure {
+  /**
+   * This procedure produces no result
+   */
+  def f(instate: (Int, Boolean)): (Int, Boolean) = {
+    val (global, flag) = instate
+    if (!flag) {
+      (global + 1, flag)
+    } else {
+      (global, flag)
+    }
+  }
+
+  /**
+   * This procedure produces one result that is of Int type
+   */
+  def g(instate: (Int, Boolean)): (Int, (Int, Boolean)) = {
+    val (global, _) = instate
+    (global, (global, true))
+  }
+
+  //this procedure produces no result
+  def havoc(instate: (Int, Boolean)): (Int, Boolean) = {
+    if (nondet[Boolean]) {
+      val next_state = f(instate)
+      havoc(next_state)
+    } else if(nondet[Boolean]) {
+      val (_, next_state) = g(instate)
+      havoc(next_state)
+    } else {
+      instate
+    }
+  } ensuring(res => !instate._2 || (res == instate))
   
-/*    def f(global: Int, flag: Boolean) : (Int, Boolean)  = {
-      if(!flag) {       
-        (global + 1, flag) 
-      } else {
-        (global, flag)
-      }
-    }
+  def init() : (Int, Boolean) = {
+    (0, false)
+  }
+
+  def purityChecker(): (Int, Int) = {
     
-    def g( global: Int, flag: Boolean) : (Int, Boolean) = {
-      (global, true)
-    }
-        
-    def havoc(global: Int, flag: Boolean) : (Int, Boolean) = {
-      
-      if(nondet) {
-        
-        val next_state = f(global, flag)
-        havoc(next_state._1, next_state._2)
-         
-      } else {
-        (global, g(global, flag)._2)
-      }
-    }
-    
-    def purityChecker(): (Int, Int) = {
-      
-      val some_state = havoc(0, false)
-      val (res1, flag1) = g(some_state._1, some_state._2)
-      val someother_state = havoc(some_state._1, flag1)
-      val res2 = g(someother_state._1, someother_state._2)._1
-      (res1, res2)
-      
-    } ensuring(res => res._1 == res._2)
-*/    
-        
-    def havoc(x: Int) : Int = {      
-      
-      val y = nondet[Boolean]
-      if(x>=0) {
-        if(y) havoc(x-1)
-        else 0
-      }       
-      else if(!y && x < 0) {
-        -1
-      } else 0
-      
-    } //ensuring(res => res >= 0)
-    
-    def purityChecker(): Int ={
-      havoc(0)
-    } ensuring(_ >= 0)
+    val some_state = havoc(init())
+    val (res1, next_state) = g(some_state)
+    val later_state = havoc(next_state)
+    val (res2, _) = g(later_state)
+    (res1, res2)
+
+  } ensuring (res => res._1 == res._2)
 }
