1e43dd2541892e7933cbb24246225a2242f72163
Ravi <ravi.kandhadai@epfl.ch>
2014-02-11 01:19:28 +0100

    updations

---
+++ examples/depthbounds/ConcatVariations.scala
@@ -0,0 +1,40 @@
+import leon.Utils._
+
+object ConcatVariations {
+  sealed abstract class List
+  case class Cons(head: Int, tail: List) extends List
+  case class Nil() extends List
+
+  def size(l: List): Int = (l match {
+    case Nil() => 0
+    case Cons(_, t) => 1 + size(t)
+  })
+
+  def genL(n: Int): List = {
+    require(n >= 0)
+    if (n == 0) Nil()
+    else
+      Cons(n, genL(n - 1))
+  } ensuring (res => size(res) == n template((a,b) => depth <= a*n + b))
+
+  def append(l1: List, l2: List): List = (l1 match {
+    case Nil() => l2
+    case Cons(x, xs) => Cons(x, append(xs, l2))
+
+  }) ensuring (res =>  size(l1) + size(l2) == size(res) template((a,b) => depth <= a*size(l1) + b))
+  
+  def f_good(m: Int, n: Int): List = {
+    require(0 <= m && 0 <= n)
+    if (m == 0) Nil()
+    else append(genL(n), f_good(m - 1, n))
+    
+  } ensuring(res => size(res) == n*m template((a,b,c,d) => depth <= a*(n*m) + b*n + c*m +d))
+
+  def f_worst(m: Int, n: Int): List = {
+    require(0 <= m && 0 <= n)
+    
+    if (m == 0) Nil()
+    else append(f_worst(m - 1, n), genL(n))
+    
+  } ensuring(res => size(res) == n*m template((a,c,d,e,f) => depth <= a*((n*m)*m)+c*(n*m)+d*n+e*m+f)) 
+}
---
+++ examples/depthbounds/MergeSort.scala
@@ -0,0 +1,56 @@
+//import scala.collection.immutable.Set
+import leon.Utils._
+import leon.Annotations._
+
+object MergeSort {
+  sealed abstract class List
+  case class Cons(head:Int,tail:List) extends List
+  case class Nil() extends List
+
+  def size(list:List): Int = {list match {
+    case Nil() => 0
+    case Cons(x,xs) => 1 + size(xs)
+  }} ensuring(res => res >= 0)
+  
+  def length(l:List): Int = {
+    l match {
+      case Nil() => 0
+      case Cons(x,xs) => 1 + length(xs)
+    }
+  } ensuring(res => res >= 0 && res == size(l) template((a,b) => depth <= a*size(l) + b))
+
+  def split(l:List,n:Int): (List,List) = {
+    require(n >= 0 && n <= size(l))    
+    if (n <= 0) (Nil(),l)
+    else
+	l match {
+      case Nil() => (Nil(),l)
+      case Cons(x,xs) => {
+        val (fst,snd) = split(xs, n-1)
+        (Cons(x,fst), snd)
+      }
+	}
+  } ensuring(res => size(res._2) == size(l) - n && size(res._1) == n template((a,b) => depth <= a*n +b))
+  
+  def merge(aList:List, bList:List):List = (bList match {       
+    case Nil() => aList
+    case Cons(x,xs) =>
+    	 aList match {
+   	       case Nil() => bList
+   	       case Cons(y,ys) =>
+    	        if (y < x)
+    		   Cons(y,merge(ys, bList))
+     		else
+		   Cons(x,merge(aList, xs))               
+   	 }   
+  }) ensuring(res => size(aList)+size(bList) == size(res) template((a,b,c) => depth <= a*size(aList) + b*size(bList) + c))  
+
+  def mergeSort(list:List):List = (list match {
+    case Nil() => list
+    case Cons(x,Nil()) => list
+    case _ =>
+    	 val (fst,snd) = split(list,length(list)/2)    	 
+   	     merge(mergeSort(fst), mergeSort(snd))
+   	 
+  }) ensuring(res => size(res) == size(list) template((a,b) => depth <= a*size(list) + b))   
+}
