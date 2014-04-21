8c5c41b816460dad46c46702c9d41f8222462fd0
ravi <ravi.kandhadai@epfl.ch>
2014-04-21 16:47:28 +0200

    Fixed one problem

--- testcases/ravi-testcases/numerical/SimpleLoop.scala
+++ testcases/ravi-testcases/numerical/SimpleLoop.scala
@@ -1,4 +1,6 @@
+import leon.lang.invariantLang._
 import leon.lang._
+import leon._
 
 object SimpleLoop
 {
@@ -7,7 +9,8 @@ object SimpleLoop
 	    0
 	  else 
 	    s(x-1) + 1	   
-	} ensuring(res => res != -1 template((a,b,c) => a*res + b*x + c <= 0)) 
+	} ensuring(res => (res != -1) template((a, b, c) => a*res + b*x + c <= 0))
+	//ensuring(res => res != -1) 
 	//ensuring(res => (res != -1) template((a, b, c) => a*res + b*x + c <= 0))	
 	//inductive generalization res >= 0	
 } 
