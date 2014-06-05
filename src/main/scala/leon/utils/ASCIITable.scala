b754c03a5f507e78ec396d3a22e628f8b459bdab
Etienne Kneuss <colder@php.net>
2014-06-05 19:05:08 +0200

    Proper handling of ASCII tables

---
+++ src/main/scala/leon/utils/ASCIITable.scala
@@ -0,0 +1,122 @@
+package leon.utils
+
+object ASCIITables {
+  case class Table(title: String, rows: Seq[TableRow] = Nil) {
+    def +(r: TableRow): Table = this ++ Seq(r)
+    def ++(rs: Iterable[TableRow]): Table = copy(rows = rows ++ rs)
+
+
+    def computeColumnSizes = {
+      // First check constraints
+      var constraints = Map[(Int, Int), Int]()
+
+      var cellsPerRow = 0
+      for((r, i) <- rows.zipWithIndex) r match {
+        case r @ Row(cells) =>
+          if (cellsPerRow > 0) {
+            assert(r.cellsSize == cellsPerRow, "Row $i has incompatible number of virtual cells (${r.cellsSize} v.s. $cellsPerRow")
+          } else {
+            cellsPerRow = r.cellsSize
+            constraints += (0, cellsPerRow-1) -> 80
+          }
+
+          var i = 0
+          for (c <- cells) {
+            val k = i -> (i+c.spanning-1)
+
+            val cols = constraints.getOrElse(k, 1)
+
+            val size = c.vString.size
+
+            constraints += k -> (cols max size)
+
+            i += c.spanning
+          }
+        case _ =>
+      }
+
+      // discharge constraints that are spanning
+      val toRemove = constraints.keySet.filter { case (from, to) => from != to }.toSeq.sortBy{ s => s._2 - s._1 }
+
+      for (k @ (from, to) <- toRemove) {
+        val min = constraints(k)
+
+        val parts = (from to to).map(i => constraints((i, i)))
+
+        var sum = parts.sum
+
+        if (sum < min) {
+          var remaining = min - sum
+
+          for ((s, i) <- parts.zipWithIndex.sortBy(- _._1)) {
+            val inc = Math.round(s*remaining*1d/sum).toInt
+            constraints += (from+i, from+i) -> (s + inc)
+            remaining -= inc
+            sum -= s
+          }
+        }
+
+        constraints -= k
+      }
+
+      (0 until cellsPerRow).map(i => constraints((i, i)))
+    }
+
+    def render: String = {
+      val colSizes = computeColumnSizes
+
+      val fullWidth = colSizes.sum + colSizes.size*2
+
+      val sb = new StringBuffer
+
+      sb append "  ┌─"+("─"*title.size)+"─┐\n"
+      sb append "╔═╡ "+      title     +" ╞" + ("═" * (fullWidth-title.size-5)) + "╗\n"
+      sb append "║ └─"+("─"*title.size)+"─┘" + (" " * (fullWidth-title.size-5)) + "║\n"
+
+      for (r <- rows) r match {
+        case Separator =>
+          sb append "╟" + ("┄" * fullWidth) + "╢\n"
+
+        case Row(cells) =>
+          sb append "║ "
+          var i = 0
+          for (c <- cells) {
+            if (i > 0) {
+              sb append "  "
+            }
+            val size = (i to i+c.spanning-1).map(colSizes).sum + (c.spanning-1) * 2
+
+            if (c.align == Left) {
+              sb append ("%-"+size+"s").format(c.vString)
+            } else {
+              sb append ("%"+size+"s").format(c.vString)
+            }
+
+            i += c.spanning
+          }
+          sb append " ║\n"
+      }
+
+      sb append "╚" + ("═" * fullWidth) + "╝"
+
+      sb.toString
+    }
+  }
+
+  abstract class TableRow
+  case object Separator extends TableRow
+  case class Row(cells: Seq[Cell]) extends TableRow {
+    def cellsSize = {
+      cells.map(_.spanning).sum
+    }
+  }
+  sealed abstract class Alignment
+  case object Left extends Alignment
+  case object Right extends Alignment
+
+
+  case class Cell(v: Any, spanning: Int = 1, align: Alignment = Left) {
+    lazy val vString = v.toString
+  }
+
+}
