c53e285ba268e9780252c1cc843fd661fbd4a29c
Regis Blanc <regwblanc@gmail.com>
2014-06-30 18:13:57 +0200

    more xlang max sum testcases

--- testcases/xlang/MaxSum.scala
+++ testcases/xlang/MaxSum.scala
@@ -35,4 +35,27 @@ object MaxSum {
     rec(0)
   }
 
+  def summ(to : Int): Int = ({
+    require(to >= 0)
+    var i = 0
+    var s = 0
+    (while (i < to) {
+      s = s + i
+      i = i + 1
+    }) invariant (s >= 0 && i >= 0 && s == i*(i-1)/2 && i <= to)
+    s
+  }) ensuring(res => res >= 0 && res == to*(to-1)/2)
+
+
+  def sumsq(to : Int): Int = ({
+    require(to >= 0)
+    var i = 0
+    var s = 0
+    (while (i < to) {
+      s = s + i*i
+      i = i + 1
+    }) invariant (s >= 0 && i >= 0 && s == (i-1)*i*(2*i-1)/6 && i <= to)
+    s
+  }) ensuring(res => res >= 0 && res == (to-1)*to*(2*to-1)/6)
+
 }
