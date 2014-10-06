051688f8fe0bd11773ebd5f0e762ff067e1fef8e
Emmanouil (Manos) Koukoutos <emmanouil.koukoutos@epfl.ch>
2014-10-06 17:59:09 +0200

    Option.get in Leon library

--- library/Option.scala
+++ library/Option.scala
@@ -6,6 +6,14 @@ import leon.annotation._
 
 @library
 sealed abstract class Option[T] {
+
+  def get : T = {
+    require(this.isDefined)
+    this match {
+      case Some(x) => x
+    }
+  }
+
   def getOrElse(default: T) = this match {
     case Some(v) => v
     case None()  => default
