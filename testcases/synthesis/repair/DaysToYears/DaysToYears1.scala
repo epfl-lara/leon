77ed38f3805fc202ffaf405eb866a239a4eb72b5
Emmanouil (Manos) Koukoutos <emmanouil.koukoutos@epfl.ch>
2014-12-03 16:56:11 +0100

    Some benchmarks

---
+++ testcases/synthesis/repair/DaysToYears/DaysToYears1.scala
@@ -0,0 +1,36 @@
+import leon.lang._
+
+object DaysToYears {
+  val base : Int = 1980
+  
+  def isLeapYear(y : Int): Boolean = y % 4 == 0
+  
+  def daysToYears(days : Int): Int = {
+    require(days > 0)
+    daysToYears1(base, days)._1
+  }
+
+  def daysToYears1(year : Int, days : Int): (Int, Int) = {
+    require(year >= base && days > 0)
+    if (days > 366 && isLeapYear(year))
+      daysToYears1(year + 1, days - 366)
+    else if (days > 365 && !isLeapYear(year))
+      daysToYears1(year, days - 365) // FIXME forgot +1
+    else (year, days)
+  } ensuring { res => 
+    res._2 <= 366 &&
+    res._2 >  0   && 
+    res._1 >= base &&
+    (((year,days), res) passes { 
+      case (1980, 366 ) => (1980, 366)
+      case (1980, 1000) => (1982, 269)
+    })
+  } 
+
+  def main(args : Array[String]) = {
+    println(daysToYears1(base, 10593 ))
+    println(daysToYears1(base, 366 ))
+    println(daysToYears1(base, 1000 ))
+  }
+}
+
