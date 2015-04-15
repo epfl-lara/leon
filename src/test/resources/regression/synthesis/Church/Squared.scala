6d108adb356845c74e20eecfa3a80a18f89eb80f
Etienne Kneuss <ekneuss@gmail.com>
2015-04-15 20:55:10 +0200

    Fix test for Church.Squared

--- src/test/resources/regression/synthesis/Church/Squared.scala
+++ src/test/resources/regression/synthesis/Church/Squared.scala
@@ -14,6 +14,11 @@ object Numerals {
     }
   } ensuring (_ >= 0)
 
+  def add(x: Num, y: Num): Num = (x match {
+    case Z => y
+    case S(p) => add(p, S(y))
+  }) ensuring (value(_) == value(x) + value(y))
+
   def mult(x: Num, y: Num): Num = (y match {
     case S(p) =>
       add(mult(x, p), x)
