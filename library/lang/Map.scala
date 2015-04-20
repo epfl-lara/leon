cdab1dac5dd82dda33f3dc8b83fb3cd936d13ebd
Etienne Kneuss <ekneuss@gmail.com>
2015-04-20 16:09:00 +0200

    Add map definition

---
+++ library/lang/Map.scala
@@ -0,0 +1,15 @@
+package leon.lang
+import leon.annotation._
+
+object Map {
+  def empty[A,B] = Map[A,B]()
+}
+
+@ignore
+case class Map[A, B](elems: (A, B)*) {
+  def apply(k: A): B = ???
+  def ++(b: Map[A, B]): Map[A,B] = ???
+  def updated(k: A, v: B): Map[A,B] = ???
+  def contains(a: A): Boolean = ???
+  def isDefinedAt(a: A): Boolean = contains(a)
+}
