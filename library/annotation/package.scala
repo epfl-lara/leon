9f90c3cf5a88a3d26e802f7b3771da963af2a614
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2015-04-27 10:24:52 +0200

    Remove legacy annotations

--- library/annotation/package.scala
+++ library/annotation/package.scala
@@ -8,20 +8,12 @@ package object annotation {
   @ignore
   class library    extends StaticAnnotation
   @ignore
-  class verified   extends StaticAnnotation
-  @ignore
   class repair     extends StaticAnnotation
-
   @ignore
   class induct     extends StaticAnnotation
-  @ignore
-  class axiomatize extends StaticAnnotation
-  @ignore
-  class main       extends StaticAnnotation
   @ignore
   class extern     extends StaticAnnotation
   @ignore
   class ignore     extends StaticAnnotation
-
 }
 
