9661794cf6486c16c1ff56bcc60953f856031d3f
Etienne Kneuss <ekneuss@gmail.com>
2015-05-06 17:49:46 +0200

    Too many successes on this one

--- travis/fetchCVC4.sh
+++ travis/fetchCVC4.sh
@@ -1,6 +1,6 @@
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 mkdir -p $dir/builds
-curl http://lara.epfl.ch/~ekneuss/cvc4-builds/cvc4-2015-05-03-x86_64-linux-opt -o $dir/builds/cvc4
+curl http://lara.epfl.ch/~ekneuss/cvc4-builds/cvc4-2015-04-17-x86_64-linux-opt -o $dir/builds/cvc4
 
 chmod u+x $dir/builds/cvc4
 
