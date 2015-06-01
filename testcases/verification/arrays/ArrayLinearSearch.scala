92d5165a8c5ea988f39d2cf9687ee7650a10a724
Regis Blanc <regwblanc@gmail.com>
2015-06-01 19:10:37 +0200

    proving property with recursive linear search

--- testcases/verification/arrays/ArrayLinearSearch.scala
+++ testcases/verification/arrays/ArrayLinearSearch.scala
@@ -6,32 +6,39 @@ object ArrayLinearSearch {
 
   def existsRec(a: Array[BigInt], x: BigInt, index: Int): Int = {
     require(
-      a.length > 0 && index >= 0 && index < a.length &&
-      arrayForall(a, 0, index, (el: BigInt) => el != x))
+      a.length > 0 && index >= 0 && index < a.length)
+      //arrayForall(a, 0, index, (el: BigInt) => el != x))
 
     if(a(index) == x)
       index
     else if(index >= a.length - 1)
-      a.length
+      -1
     else
       existsRec(a, x, index+1)
     
   } ensuring(pos => {
-      //arrayForall(a, 0, pos, (el: BigInt) => el != x)
+      if(pos == -1)
+        arrayForall(a, index, a.length, (el: BigInt) => el != x)
+      else
+        pos >= index && 
+        pos < a.length &&
+        a(pos) == x
+  })
+        
 
       //arrayForall(a, 0, pos, (el: BigInt) => el != x) &&
-      (if(a(index) == x) {
-        arrayForall(a, 0, pos, (el: BigInt) => el != x) &&
-        a(index) == x &&
-        index == pos
-      } else if(index >= a.length - 1) {
-        index == a.length - 1 &&
-        pos == a.length &&
-        arrayForall(a, 0, pos, (el: BigInt) => el != x) &&
-        a(a.length - 1) != x
-      } else {
-        arrayForall(a, 0, index+1, (el: BigInt) => el != x) &&
-        arrayForall(a, 0, pos, (el: BigInt) => el != x)
+      //(if(a(index) == x) {
+      //  arrayForall(a, 0, pos, (el: BigInt) => el != x) &&
+      //  a(index) == x &&
+      //  index == pos
+      //} else if(index >= a.length - 1) {
+      //  index == a.length - 1 &&
+      //  pos == a.length &&
+      //  arrayForall(a, 0, pos, (el: BigInt) => el != x) &&
+      //  a(a.length - 1) != x
+      //} else {
+      //  arrayForall(a, 0, index+1, (el: BigInt) => el != x) &&
+      //  arrayForall(a, 0, pos, (el: BigInt) => el != x)
         //pos == existsRec(a, x, index+1) &&
         //index < a.length - 1 &&
         //a(index) != x &&
@@ -47,13 +54,18 @@ object ArrayLinearSearch {
         // else 
         //  arrayForall(a, 0, pos, (el: BigInt) => el != x)
         //)
-      })
-    })
+      //})
       
 
   def exists(a: Array[BigInt], x: BigInt): Int = {
     require(a.length > 0)
     existsRec(a, x, 0)
-  } ensuring(index => arrayForall(a, 0, index, (el: BigInt) => el != x))
+  } ensuring(index => {
+    if(index == -1)
+      arrayForall(a, (el: BigInt) => el != x)
+    else
+      a(index) == x
+  })
+
 
 }
