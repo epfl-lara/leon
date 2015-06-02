c015b44fa568964c5bd1b5de0f72117fd6ba9f2d
Samuel Gruetter <samuel.gruetter@epfl.ch>
2015-06-02 13:12:30 +0200

    --verify --termination does verification and termination checking in one run

--- src/main/scala/leon/Pipeline.scala
+++ src/main/scala/leon/Pipeline.scala
@@ -15,3 +15,16 @@ abstract class Pipeline[-F, +T] {
 
   def run(ctx: LeonContext)(v: F): T
 }
+
+object Pipeline {
+  
+  def both[T, R1, R2](f1: Pipeline[T, R1], f2: Pipeline[T, R2]): Pipeline[T, (R1, R2)] = new Pipeline[T, (R1, R2)] {
+    def run(ctx : LeonContext)(t : T): (R1, R2) = {
+      val r1 = f1.run(ctx)(t)
+      // don't check for SharedOptions.optStrictPhases because f2 does not depend on the result of f1
+      val r2 = f2.run(ctx)(t)
+      (r1, r2)
+    }
+  }
+  
+}
