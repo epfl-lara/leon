58d3caee6697cd84c4ab466f10933277d735a285
Regis Blanc <regwblanc@gmail.com>
2015-06-03 18:50:39 +0200

    cleaner spec for binary search

--- testcases/verification/xlang/arrays/BinarySearch.scala
+++ testcases/verification/xlang/arrays/BinarySearch.scala
@@ -5,7 +5,7 @@ import leon.lang._
 object BinarySearch {
 
   def binarySearch(a: Array[BigInt], key: BigInt): Int = ({
-    require(a.length > 0 && sorted(a, 0, a.length - 1))
+    require(a.length > 0 && isSorted(a, 0, a.length - 1))
 
     var low = 0
     var high = a.length - 1
@@ -23,31 +23,25 @@ object BinarySearch {
       else if(v < key)
         low = i + 1
     }) invariant(
-        res >= -1 &&
-        res < a.length &&
-        0 <= low && 
-        low <= high + 1 && 
-        high >= -1 &&
-        high < a.length && 
-        (if (res >= 0) 
-            a(res) == key else 
-            (!occurs(a, 0, low, key) && !occurs(a, high + 1, a.length, key))
+        0 <= low && 0 <= high && low <= high + 1 && high < a.length &&
+        (if(res == -1)
+          arrayForall(a, 0, low, (el: BigInt) => el != key) && 
+          arrayForall(a, high+1, a.length, (el: BigInt) => el != key)
+        else
+          res >= 0 && res < a.length &&
+          a(res) == key)
         )
-       )
     res
-  }) ensuring(res => 
-      res >= -1 && 
-      res < a.length && 
-      (if(res >= 0) a(res) == key else !occurs(a, 0, a.length, key)))
+  }) ensuring(res => {
+      if(res == -1)
+        arrayForall(a, (el: BigInt) => el != key)
+      else
+        a(res) == key
+     })
 
-
-  def occurs(a: Array[BigInt], from: Int, to: Int, key: BigInt): Boolean = {
-    require(a.length >= 0 && to <= a.length && from >= 0)
-    arrayExists(a, from, to, (el: BigInt) => el == key)
-  }
-
-  def sorted(a: Array[BigInt], l: Int, u: Int): Boolean = {
+  def isSorted(a: Array[BigInt], l: Int, u: Int): Boolean = {
     require(a.length > 0 && l >= 0 && u < a.length && l <= u)
     boundedForall(l, u, (i: Int) => a(i) <= a(i+1))
   }
+
 }
