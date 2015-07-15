96ce1929f28eab449149ad5b66479394c7b59130
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2015-07-15 14:52:05 +0200

    Test unapply-synthesis interaction

---
+++ src/test/resources/regression/synthesis/Misc/Unapply.scala
@@ -0,0 +1,16 @@
+import leon.lang._ 
+import leon.lang.synthesis._
+
+object Unap {
+  def unapply[A, B](i: (Int, B, A)): Option[(A, B)] = 
+    if (i._1 == 0) None() else Some((i._3, i._2))
+}
+
+object Unapply {
+  def bar(i: Int, b: Boolean): Boolean = (i, b, ()) match {
+    case Unap(_, b) if b => b
+    case Unap((), b) => 
+      choose( (b1: Boolean) => b == b1 == i < 0 )
+  }
+
+}
