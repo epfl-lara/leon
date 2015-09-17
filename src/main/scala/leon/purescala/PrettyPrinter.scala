0dc88dde228e80ef8d9986c98b8cbd58c9697d99
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2015-09-17 15:39:10 +0200

    Maybe printing everything when debugging is a bad idea

--- src/main/scala/leon/purescala/PrettyPrinter.scala
+++ src/main/scala/leon/purescala/PrettyPrinter.scala
@@ -408,7 +408,7 @@ class PrettyPrinter(opts: PrinterOptions,
 
       // Definitions
       case Program(units) =>
-        p"""${nary(units filter { opts.printUniqueIds || _.isMainUnit }, "\n\n")}"""
+        p"""${nary(units filter { /*opts.printUniqueIds ||*/ _.isMainUnit }, "\n\n")}"""
 
       case UnitDef(id,pack, imports, defs,_) =>
         if (pack.nonEmpty){
