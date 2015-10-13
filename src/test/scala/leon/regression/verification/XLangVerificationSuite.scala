a791ddb82699ce2e79167cbd974d48f3544549a1
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2015-10-13 14:51:58 +0200

    XLangVerificationSuite checks for z3

--- src/test/scala/leon/regression/verification/XLangVerificationSuite.scala
+++ src/test/scala/leon/regression/verification/XLangVerificationSuite.scala
@@ -2,13 +2,29 @@
 
 package leon.regression.verification
 
+import smtlib.interpreters.Z3Interpreter
+
 class XLangVerificationSuite extends VerificationSuite {
 
-  val optionVariants: List[List[String]] = List(
-    List(),
-    List("--feelinglucky"),
-    List("--solvers=smt-z3", "--feelinglucky")
-  )
+  val optionVariants: List[List[String]] = {
+    val isZ3Available = try {
+      Z3Interpreter.buildDefault.free()
+      true
+    } catch {
+      case e: java.io.IOException =>
+        false
+    }
+
+    List(
+      List(),
+      List("--feelinglucky")
+    ) ++ (
+      if (isZ3Available)
+        List(List("--solvers=smt-z3", "--feelinglucky"))
+      else Nil
+    )
+  }
+
   val testDir: String = "regression/verification/xlang/"
   override val desugarXLang = true
 }
