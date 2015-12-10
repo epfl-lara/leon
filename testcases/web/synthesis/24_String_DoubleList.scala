6703c75eb9e1ba8e50d1b84d98eed35019d8aef6
Mikaël Mayer <a-mikmay@microsoft.com>
2015-12-10 14:39:35 +0100

    Better test cases for string synthesis

---
+++ testcases/web/synthesis/24_String_DoubleList.scala
@@ -0,0 +1,29 @@
+import leon.lang._
+import leon.annotation._
+import leon.collection._
+import leon.collection.ListOps._
+import leon.lang.synthesis._
+
+object DoubleListRender {
+  abstract class A
+  case class B(i: AA, a: A) extends A
+  case class N() extends A
+  
+  abstract class AA
+  case class BB(i: A, a: AA) extends AA
+  case class NN() extends AA
+  
+  // Obviously wrong, but it is useful to use the display function.
+  def twoOutOfThreeAsAreTheSame(a: A, b: A, c: A): Boolean = {
+    a == b || a == c || b == c
+  } holds
+
+  def AtoString(a : A): String =  {
+    ???
+  } ensuring {
+    (res : String) => (a, res) passes {
+      case B(BB(N(), BB(B(NN(), B(NN(), N())), NN())), N()) =>
+        "[([], [(), ()])]"
+    }
+  }
+}
