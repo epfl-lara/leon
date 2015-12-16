a6ab44ad2bd69c8e1e4b9cff7b0bb45f5aeaed48
Marco Antognini <antognini.marco@gmail.com>
2015-12-16 19:16:08 +0100

    Add support for case class

---
+++ src/test/resources/regression/genc/valid/CaseClass.scala
@@ -0,0 +1,19 @@
+import leon.lang._
+
+object CaseClass {
+
+  case class Color(r: Int, g: Int, b: Int)
+
+  def red  = Color(0, 255, 0)
+  def cyan = Color(0, 255, 255)
+
+  def sub(c: Color, d: Color) = Color(c.r - d.r, c.g - d.g, c.b - d.b)
+
+  def main = {
+    val c = red
+    val d = cyan
+    val z = sub(c, d).g
+    z
+  }
+
+}
