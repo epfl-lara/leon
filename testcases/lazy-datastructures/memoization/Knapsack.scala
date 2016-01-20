daadeb20b080831d560f3b0175bde67874cc7248
ravi <ravi.kandhadai@epfl.ch>
2016-01-20 12:31:03 +0100

    Proving Knapscak. But it is a little messy

--- testcases/lazy-datastructures/memoization/Knapsack.scala
+++ testcases/lazy-datastructures/memoization/Knapsack.scala
@@ -6,16 +6,24 @@ import leon.instrumentation._
 //import leon.invariant._
 
 object Knapscak {
-  sealed abstract class IList
+  sealed abstract class IList {
+    def size: BigInt = {
+      this match {
+        case Cons(_, tail) => 1 + tail.size
+        case Nil() => BigInt(0)
+      }
+    } ensuring(_ >= 0)
+  }
   case class Cons(x: BigInt, tail: IList) extends IList
   case class Nil() extends IList
 
   def depsEval(i: BigInt, items: IList): Boolean = {
     require(i >= 0)
-    if (i <= 0) true
-    else {
-      knapSack(i, items).isCached && depsEval(i - 1, items)
-    }
+    knapSack(i, items).isCached && // if we have the cached check only along the else branch, we would get a counter-example.
+      (if (i <= 0) true
+      else {
+        depsEval(i - 1, items)
+      })
   }
 
   @traceInduct
@@ -30,8 +38,9 @@ object Knapscak {
     (x <= y && depsEval(y, items)) ==> depsEval(x, items)
   } holds
 
+  @invstate
   def maxValue(items: IList, w: BigInt, currList: IList): BigInt = {
-    require((w >= 0 || (w > 0 && depsEval(w - 1, items))) &&
+    require((w ==0 || (w > 0 && depsEval(w - 1, items))) &&
       // lemma inst
       (currList match {
         case Cons(wi, _) =>
@@ -51,7 +60,7 @@ object Knapscak {
         } else oldMax
       case Nil() => BigInt(0)
     }
-  }
+  } ensuring(_ => time <= 30*currList.size + 20) // interchanging currList and items in the bound will produce a counter-example
 
   @memoize
   def knapSack(w: BigInt, items: IList): BigInt = {
@@ -60,7 +69,7 @@ object Knapscak {
     else {
       maxValue(items, w, items)
     }
-  }
+  } ensuring(_ => time <= 30*items.size + 25)
 
   def invoke(i: BigInt, items: IList) = {
     require(i == 0 || (i > 0 && depsEval(i - 1, items)))
@@ -69,11 +78,12 @@ object Knapscak {
     val in = $.inState[BigInt]
     val out = $.outState[BigInt]
     depsEvalMono(i - 1, items, in, out) &&
-      depsEval(i - 1, items)
+      depsEval(i - 1, items) &&
+      time <= 30*items.size + 40
   })
 
   def bottomup(i: BigInt, w: BigInt, items: IList): IList = {
-    require(w >= 0 &&
+    require(i >= 0 && w >= i &&
       (i == 0 || (i > 0 && depsEval(i - 1, items))))
     val ri = invoke(i, items)
     if (i == w)
@@ -81,5 +91,11 @@ object Knapscak {
     else {
       Cons(ri, bottomup(i + 1, w, items))
     }
-  }
+  } ensuring(_ => items.size <= 10 ==> time <= 400 * (w - i + 1))
+
+  def knapSackSol(w: BigInt, items: IList) = {
+    require(w >= 0 && items.size <= 10) //  the second requirement is only to keep the bounds linear for z3 to work
+    bottomup(0, w, items)
+  } ensuring(time <= 400*w + 410)
+
 }
