aff8c520b4ba07f43a74bfffaee917c908e92a35
Mikaël Mayer <mikael.mayer@epfl.ch>
2016-03-09 16:12:14 +0100

    Webpage now working

--- library/webDSL/WebPage.scala
+++ library/webDSL/WebPage.scala
@@ -4,15 +4,15 @@ package webDSL
   * Created by dupriez on 3/1/16.
   */
 //TODO: Check if a WebPage is pickable by boopickle. I remember that boopickle can only pickle case classes, and inheritance of case classes is prohibited
-sealed trait WebPage {
+sealed trait WebPage/* {
 //  val content: List[WebElement] = List()
   val content: leon.collection.List[Int] = leon.collection.List()
   var testString = "Coucou"
-}
+}*/
 
-case object ErrorWebPage extends WebPage {
+case object ErrorWebPage extends WebPage/* {
 
-}
+}*/
 
-case class BlankWebPage(sons: leon.collection.List[WebAttribute]) extends WebPage {
-}
+case class BlankWebPage(sons: leon.collection.List[WebAttribute]) extends WebPage /*{
+}*/
