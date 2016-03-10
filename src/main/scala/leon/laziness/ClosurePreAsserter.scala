f00333041750454e4977311d1b13ff2387c55183
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2016-03-10 14:35:00 +0100

    Remove unused commits mostly

--- src/main/scala/leon/laziness/ClosurePreAsserter.scala
+++ src/main/scala/leon/laziness/ClosurePreAsserter.scala
@@ -1,20 +1,15 @@
 package leon
 package laziness
 
-import invariant.factories._
-import invariant.util.Util._
 import invariant.util._
 import invariant.structure.FunctionUtils._
-import purescala.ScalaPrinter
 import purescala.Common._
 import purescala.Definitions._
 import purescala.Expressions._
 import purescala.ExprOps._
 import purescala.DefOps._
-import purescala.Extractors._
 import purescala.Types._
 import leon.invariant.util.TypeUtil._
-import leon.invariant.util.LetTupleSimplification._
 import leon.invariant.util.ProgramUtil._
 import leon.invariant.util.PredicateUtil._
 import LazinessUtil._
