b7acab9a666dc73b49881019fb5b0d446470df4b
Marco Antognini <antognini.marco@gmail.com>
2016-04-07 15:20:31 +0200

    Update CaseClass test with member function

--- src/test/resources/regression/genc/valid/CaseClass.scala
+++ src/test/resources/regression/genc/valid/CaseClass.scala
@@ -4,18 +4,22 @@ import leon.lang._
 
 object CaseClass {
 
-  case class Color(r: Int, g: Int, b: Int)
+  case class Color(r: Int, g: Int, b: Int) {
+    def getR = r
+  }
 
-  def red  = Color(0, 255, 0)
-  def cyan = Color(0, 255, 255)
+  def green = Color(0, 255, 0)
+  def cyan  = Color(0, 255, 255)
 
   def sub(c: Color, d: Color) = Color(c.r - d.r, c.g - d.g, c.b - d.b)
 
   def main = {
-    val c = red
+    val c = green
     val d = cyan
     val z = sub(c, d).g
-    z
+
+    if (c.getR == 0) z
+    else -1
   } ensuring { _ == 0 }
 
 }
