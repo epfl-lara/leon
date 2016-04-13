045a7274de2c8071459919ce5884060430b44ac6
Regis Blanc <regwblanc@gmail.com>
2016-04-13 16:42:56 +0200

    fix extractors for Arrays

--- src/test/scala/leon/unit/purescala/ExtractorsSuite.scala
+++ src/test/scala/leon/unit/purescala/ExtractorsSuite.scala
@@ -25,17 +25,24 @@ class ExtractorsSuite extends FunSuite with ExpressionsDSL {
     assert(e3 === e4)
   }
 
-  ignore("Extractors of NonemptyArray with sparse elements") {
+  test("Extractors of NonemptyArray with sparse elements") {
     val a1 = NonemptyArray(Map(0 -> x, 3 -> y, 5 -> z), Some((BigInt(0), BigInt(10))))
-
     val a2 = a1 match {
       case Operator(es, builder) => {
         assert(es === Seq(x, y, z, InfiniteIntegerLiteral(0), InfiniteIntegerLiteral(10)))
         builder(es)
       }
     }
-
     assert(a2 === a1)
+
+    val a3 = NonemptyArray(Map(0 -> x, 1 -> y, 2 -> z), None)
+    val a4 = a3 match {
+      case Operator(es, builder) => {
+        assert(es === Seq(x, y, z))
+        builder(es)
+      }
+    }
+    assert(a3 === a4)
   }
 
 }
