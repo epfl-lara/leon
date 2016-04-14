345c72bac9dfa869399dd4137d603bd88702da44
Mikaël Mayer <mikael.mayer@epfl.ch>
2016-04-14 13:00:25 +0200

    Added MatchExpr coverage test. Replaced Seq[Identifier] with Option[Seq[Identifier]] to take into account transformed expressions without new identifiers.

--- src/main/scala/leon/synthesis/disambiguation/InputCoverage.scala
+++ src/main/scala/leon/synthesis/disambiguation/InputCoverage.scala
@@ -32,77 +32,103 @@ class InputCoverage(fd: FunDef, fds: Set[FunDef])(implicit c: LeonContext, p: Pr
   
   /** If the sub-branches contain identifiers, it returns them unchanged.
       Else it creates a new boolean indicating this branch. */
-  def wrapBranch(e: (Expr, Seq[Identifier])): (Expr, Seq[Identifier]) = {
-    if(e._2.isEmpty) {
-      val b = FreshIdentifier("l" + e._1.getPos.line + "c" + e._1.getPos.col, BooleanType)
-      (tupleWrap(Seq(e._1, Variable(b))), Seq(b))
-    } else e // No need to introduce a new boolean since if one of the child booleans is true, then this IfExpr has been called.
+  def wrapBranch(e: (Expr, Option[Seq[Identifier]])): (Expr, Option[Seq[Identifier]]) = e._2 match {
+    case None =>
+      val b = FreshIdentifier("l" + Math.abs(e._1.getPos.line) + "c" + Math.abs(e._1.getPos.col), BooleanType)
+      (tupleWrap(Seq(e._1, Variable(b))), Some(Seq(b)))
+    case Some(Seq()) =>
+      val b = FreshIdentifier("l" + Math.abs(e._1.getPos.line) + "c" + Math.abs(e._1.getPos.col), BooleanType)
+      
+      def putInLastBody(e: Expr): Expr = e match {
+        case Tuple(Seq(v, prev_b)) => Tuple(Seq(v, or(prev_b, b.toVariable))).copiedFrom(e)
+        case LetTuple(binders, value, body) => letTuple(binders, value, putInLastBody(body)).copiedFrom(e)
+        case MatchExpr(scrut, Seq(MatchCase(TuplePattern(optId, binders), None, rhs))) => 
+          MatchExpr(scrut, Seq(MatchCase(TuplePattern(optId, binders), None, putInLastBody(rhs)))).copiedFrom(e)
+        case _ => throw new Exception(s"Unexpected branching case: $e")
+        
+      }
+      (putInLastBody(e._1), Some(Seq(b)))
+    case _ =>
+      // No need to introduce a new boolean since if one of the child booleans is true, then this IfExpr has been called.
+      e
   }
   
   def hasConditionals(e: Expr) = {
     ExprOps.exists{ case i:IfExpr => true case m: MatchExpr => true case f: FunctionInvocation => true case _ => false}(e)
   }
   
+  def merge(a: Option[Seq[Identifier]], b: Option[Seq[Identifier]]) = {
+    (a, b) match {
+      case (None, None) => None
+      case (a, None) => a
+      case (None, b) => b
+      case (Some(a), Some(b)) => Some(a ++ b)
+    }
+  }
+  
   /** For each branch in the expression, adds a boolean variable such that the new type of the expression is (previousType, Boolean)
    *  If no variable is output, then the type of the expression is not changed.
-    * Returns the list of boolean variables which appear in the expression */
+    * If the expression is augmented with a boolean, returns the list of boolean variables which appear in the expression */
   // All functions now return a boolean along with their original return type.
-  def markBranches(e: Expr): (Expr, Seq[Identifier]) =
-    if(!hasConditionals(e)) (e, Seq()) else e match {
+  def markBranches(e: Expr): (Expr, Option[Seq[Identifier]]) =
+    if(!hasConditionals(e)) (e, None) else e match {
     case IfExpr(cond, thenn, elze) =>
       val (c1, cv1) = markBranches(cond)
       val (t1, tv1) = wrapBranch(markBranches(thenn))
       val (e1, ev1) = wrapBranch(markBranches(elze))
-      if(cv1.isEmpty) {
-        (IfExpr(c1, t1, e1).copiedFrom(e), tv1 ++ ev1)
-      } else {
-        val arg_id = FreshIdentifier("arg", BooleanType)
-        val arg_b = FreshIdentifier("b", BooleanType)
-        (letTuple(Seq(arg_id, arg_b), c1, IfExpr(Variable(arg_id), t1, e1).copiedFrom(e)), cv1 ++ tv1 ++ ev1)
+      cv1 match {
+        case None =>
+          (IfExpr(c1, t1, e1).copiedFrom(e), merge(tv1, ev1))
+        case cv1 =>
+          val arg_id = FreshIdentifier("arg", BooleanType)
+          val arg_b = FreshIdentifier("bc", BooleanType)
+          (letTuple(Seq(arg_id, arg_b), c1, IfExpr(Variable(arg_id), t1, e1).copiedFrom(e)).copiedFrom(e), merge(merge(cv1, tv1), ev1))
       }
     case MatchExpr(scrut, cases) =>
       val (c1, cv1) = markBranches(scrut)
-      val (new_cases, variables) = (cases map { case MatchCase(pattern, opt, rhs) =>
+      val (new_cases, variables) = (cases map { case m@MatchCase(pattern, opt, rhs) =>
         val (rhs_new, ids) = wrapBranch(markBranches(rhs))
-        (MatchCase(pattern, opt, rhs_new), ids)
+        (MatchCase(pattern, opt, rhs_new).copiedFrom(m), ids)
       }).unzip // TODO: Check for unapply with function pattern ?
-      (MatchExpr(c1, new_cases).copiedFrom(e), variables.flatten)
+      (MatchExpr(c1, new_cases).copiedFrom(e), variables.fold(None)(merge))
     case Operator(lhsrhs, builder) =>
       // The exprBuilder adds variable definitions needed to compute the arguments.
-      val (exprBuilder, children, ids) = (((e: Expr) => e, List[Expr](), ListBuffer[Identifier]()) /: lhsrhs) {
-        case ((exprBuilder, children, ids), arg) =>
+      val (exprBuilder, children, tmpIds, ids) = (((e: Expr) => e, ListBuffer[Expr](), ListBuffer[Identifier](), None: Option[Seq[Identifier]]) /: lhsrhs) {
+        case ((exprBuilder, children, tmpIds, ids), arg) =>
           val (arg1, argv1) = markBranches(arg)
-          if(argv1.nonEmpty) {
+          if(argv1.nonEmpty || isNewFunCall(arg1)) {
             val arg_id = FreshIdentifier("arg", arg.getType)
-            val arg_b = FreshIdentifier("b", BooleanType)
-            val f = (body: Expr) => letTuple(Seq(arg_id, arg_b), arg1, body)
-            (exprBuilder andThen f, Variable(arg_id)::children, ids ++= argv1)
+            val arg_b = FreshIdentifier("ba", BooleanType)
+            val f = (body: Expr) => letTuple(Seq(arg_id, arg_b), arg1, body).copiedFrom(body)
+            (exprBuilder andThen f, children += Variable(arg_id), tmpIds += arg_b, merge(ids, argv1))
           } else {
-            (exprBuilder, arg::children, ids)
+            (exprBuilder, children += arg, tmpIds, ids)
           }
       }
       e match {
-        case FunctionInvocation(TypedFunDef(fd, targs), args) if fds(fd) =>
+        case FunctionInvocation(tfd@TypedFunDef(fd, targs), args) if fds(fd) =>
           val new_fd = wrapFunDef(fd)
           // Is different since functions will return a boolean as well.
-          val res_id = FreshIdentifier("res", fd.returnType)
-          val res_b = FreshIdentifier("b", BooleanType)
-          if(ids.isEmpty) {
-            val funCall = FunctionInvocation(TypedFunDef(new_fd, targs), children).copiedFrom(e)
-            (exprBuilder(funCall), Seq(res_b))
-          } else {
-            val finalIds = (ids :+ res_b)
-            val finalExpr = 
-              tupleWrap(Seq(Variable(res_id), or(finalIds.map(Variable(_)): _*)))
-            val funCall = letTuple(Seq(res_id, res_b), FunctionInvocation(TypedFunDef(new_fd, targs), children).copiedFrom(e), finalExpr)
-            (exprBuilder(funCall), finalIds)
+          tmpIds match {
+            case Seq() =>
+              val funCall = FunctionInvocation(TypedFunDef(new_fd, targs).copiedFrom(tfd), children).copiedFrom(e)
+              (exprBuilder(funCall), if(new_fd != fd) merge(Some(Seq()), ids) else ids)
+            case idvars =>
+              val res_id = FreshIdentifier("res", fd.returnType)
+              val res_b = FreshIdentifier("bb", BooleanType)
+              val finalIds = idvars :+ res_b
+              val finalExpr = 
+                tupleWrap(Seq(Variable(res_id), or(finalIds.map(Variable(_)): _*))).copiedFrom(e)
+              val funCall = letTuple(Seq(res_id, res_b), FunctionInvocation(TypedFunDef(new_fd, targs), children).copiedFrom(e), finalExpr).copiedFrom(e)
+              (exprBuilder(funCall), Some(finalIds))
           }
         case _ =>
-          if(ids.isEmpty) {
-            (e, Seq.empty)
-          } else {
-            val finalExpr = tupleWrap(Seq(builder(children).copiedFrom(e), or(ids.map(Variable): _*)))
-            (exprBuilder(finalExpr), ids)
+          tmpIds match {
+            case Seq() =>
+              (e, ids)
+            case idvars =>
+              val finalExpr = tupleWrap(Seq(builder(children).copiedFrom(e), or(idvars.map(Variable): _*))).copiedFrom(e)
+              (exprBuilder(finalExpr), ids)
           }
       }
   }
@@ -111,11 +137,19 @@ class InputCoverage(fd: FunDef, fds: Set[FunDef])(implicit c: LeonContext, p: Pr
   
   def wrapFunDef(fd: FunDef): FunDef = {
     if(!(cache contains fd)) {
-      val new_fd = fd.duplicate(returnType = TupleType(Seq(fd.returnType, BooleanType)))
-      new_fd.body = None
-      cache += fd -> new_fd
+      cache += fd -> (if(fds(fd)) {
+        val new_fd = fd.duplicate(returnType = TupleType(Seq(fd.returnType, BooleanType)))
+        new_fd.body = None
+        new_fd
+      } else fd)
     }
     cache(fd)
+  }
+  
+  def isNewFunCall(e: Expr): Boolean = e match {
+    case FunctionInvocation(TypedFunDef(fd, targs), args) =>
+      cache.values.exists { f => f == fd }
+    case _ => false
   }
   
   /** The number of expressions is the same as the number of arguments. */
