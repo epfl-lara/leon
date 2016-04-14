57935b5ef830ff877fc28ec15f7b69fa8bd90db4
Mikaël Mayer <mikael.mayer@epfl.ch>
2016-04-14 13:00:25 +0200

    Added MatchExpr coverage test. Replaced Seq[Identifier] with Option[Seq[Identifier]] to take into account transformed expressions without new identifiers.

--- src/test/scala/leon/integration/solvers/InputCoverageSuite.scala
+++ src/test/scala/leon/integration/solvers/InputCoverageSuite.scala
@@ -46,6 +46,7 @@ import leon.test.helpers.ExpressionsDSL
 import leon.synthesis.disambiguation.InputCoverage
 import leon.test.helpers.ExpressionsDSLProgram
 import leon.test.helpers.ExpressionsDSLVariables
+import leon.purescala.Extractors._
 
 class InputCoverageSuite extends LeonTestSuiteWithProgram with Matchers with ScalaFutures with ExpressionsDSLProgram with ExpressionsDSLVariables {
   val sources = List("""
@@ -63,11 +64,26 @@ class InputCoverageSuite extends LeonTestSuiteWithProgram with Matchers with Sca
     |      2
     |    }
     |  }
+    |  
     |  def withIfInIf(cond: Boolean) = {
     |    if(if(cond) false else true) {
     |      1
     |    } else {
     |      2
+    |    }
+    |  }
+    |  
+    |  sealed abstract class A
+    |  case class B() extends A
+    |  case class C(a: Int, tail: A) extends A
+    |  case class D(a: String, tail: A, b: String) extends A
+    |  
+    |  def withMatch(a: A): String = {
+    |    a match {
+    |      case B() => "B"
+    |      case C(a, C(b, tail)) => b.toString + withMatch(tail) + a.toString
+    |      case C(a, tail) => withMatch(tail) + a.toString
+    |      case D(a, tail, b) => a + withMatch(tail) + b
     |    }
     |  }
     |  
@@ -93,10 +109,14 @@ class InputCoverageSuite extends LeonTestSuiteWithProgram with Matchers with Sca
     val dummy = funDef("InputCoverageSuite.dummy")
     val coverage = new InputCoverage(dummy, Set(dummy))
     val simpleExpr = Plus(IntLiteral(1), b)
-    coverage.wrapBranch((simpleExpr, Seq(p.id, q.id))) should equal ((simpleExpr, Seq(p.id, q.id)))
-    val (covered, ids) = coverage.wrapBranch((simpleExpr, Seq()))
-    ids should have size 1
-    covered should equal (Tuple(Seq(simpleExpr, Variable(ids.head))))
+    coverage.wrapBranch((simpleExpr, Some(Seq(p.id, q.id)))) should equal ((simpleExpr, Some(Seq(p.id, q.id))))
+    coverage.wrapBranch((simpleExpr, None)) match {
+      case (covered, Some(ids)) =>
+        ids should have size 1
+        covered should equal (Tuple(Seq(simpleExpr, Variable(ids.head))))
+      case _ =>
+        fail("No ids added")
+    }
   }
   
   test("If-coverage should work"){ ctxprogram =>
@@ -105,12 +125,16 @@ class InputCoverageSuite extends LeonTestSuiteWithProgram with Matchers with Sca
     val coverage = new InputCoverage(withIf, Set(withIf))
     val expr = withIf.body.get
     
-    val (res, ids) = coverage.markBranches(expr)
-    ids should have size 2
-    expr match {
-      case IfExpr(cond, thenn, elze) =>
-        res should equal (IfExpr(cond, Tuple(Seq(thenn, Variable(ids(0)))), Tuple(Seq(elze, Variable(ids(1))))))
-      case _ => fail(s"$expr is not an IfExpr")
+    coverage.markBranches(expr) match {
+      case (res, Some(ids)) =>
+        ids should have size 2
+        expr match {
+          case IfExpr(cond, thenn, elze) =>
+            res should equal (IfExpr(cond, Tuple(Seq(thenn, Variable(ids(0)))), Tuple(Seq(elze, Variable(ids(1))))))
+          case _ => fail(s"$expr is not an IfExpr")
+        }
+      case _ =>
+        fail("No ids added")
     }
   }
   
@@ -120,15 +144,52 @@ class InputCoverageSuite extends LeonTestSuiteWithProgram with Matchers with Sca
     val coverage = new InputCoverage(withIfInIf, Set(withIfInIf))
     val expr = withIfInIf.body.get
     
-    val (res, ids) = coverage.markBranches(expr)
-    ids should have size 4
-    expr match {
-      case IfExpr(IfExpr(cond, t1, e1), t2, e2) =>
-        res match {
-          case MatchExpr(IfExpr(c, t1, e1), Seq(MatchCase(TuplePattern(None, s), None, IfExpr(c2, t2, e2)))) if s.size == 2 =>
-          case _ => fail("should have a shape like (if() else ) match { case (a, b) => if(...) else }")
+    coverage.markBranches(expr) match {
+      case (res, None) => fail("No ids added")
+      case (res, Some(ids)) =>
+        ids should have size 4
+        expr match {
+          case IfExpr(IfExpr(cond, t1, e1), t2, e2) =>
+            res match {
+              case MatchExpr(IfExpr(c, t1, e1), Seq(MatchCase(TuplePattern(None, s), None, IfExpr(c2, t2, e2)))) if s.size == 2 =>
+              case _ => fail("should have a shape like (if() else ) match { case (a, b) => if(...) else }")
+            }
+          case _ => fail(s"$expr is not an IfExpr")
         }
-      case _ => fail(s"$expr is not an IfExpr")
+    }
+  }
+  
+  test("Match coverage should work with recursive functions") { ctxprogram =>
+    implicit val (c, p) = ctxprogram
+    val withMatch = funDef("InputCoverageSuite.withMatch")
+    val coverage = new InputCoverage(withMatch, Set(withMatch))
+    val expr = withMatch.body.get
+    
+    coverage.markBranches(expr) match {
+      case (res, None) => fail("No ids added")
+      case (res, Some(ids)) =>
+        withClue(res.toString) {
+          ids should have size 4
+          res match {
+            case MatchExpr(scrut,
+                   Seq(
+                       MatchCase(CaseClassPattern(_, _, Seq()), None, rhs1),
+                       MatchCase(CaseClassPattern(_, _, _), None, rhs2),
+                       MatchCase(CaseClassPattern(_, _, _), None, rhs3),
+                       MatchCase(CaseClassPattern(_, _, _), None, rhs4))
+            ) =>
+              rhs1 match {
+                case Tuple(Seq(_, Variable(b))) => b shouldEqual ids(0)
+                case _ => fail(s"$rhs1 should be a Tuple")
+              }
+              rhs2 match {
+                case LetTuple(_, _, Tuple(Seq(_, Or(Seq(_, Variable(b)))))) => b shouldEqual ids(1)
+                case _ => fail(s"$rhs2 should be a val + tuple like val ... = ... ; (..., ... || ${ids(1)})")
+              }
+              
+            case _ => fail(s"$res does not have the format a match { case B() => .... x 4 }")
+          }
+        }
     }
   }
   
@@ -139,24 +200,28 @@ class InputCoverageSuite extends LeonTestSuiteWithProgram with Matchers with Sca
     val coverage = new InputCoverage(withCoveredFun1, Set(withCoveredFun1, withCoveredFun2))
     val expr = withCoveredFun1.body.get
     
-    val (res, ids) = coverage.markBranches(expr)
-    ids should have size 2
-    
-    res match {
-      case MatchExpr(funCall, Seq(
-            MatchCase(TuplePattern(None, Seq(WildcardPattern(Some(a1)), WildcardPattern(Some(b1)))), None,
-              MatchExpr(funCall2, Seq(
-                MatchCase(TuplePattern(None, Seq(WildcardPattern(Some(a2)), WildcardPattern(Some(b2)))), None,
-                  Tuple(Seq(BVPlus(Variable(ida1), Variable(ida2)), Or(Seq(Variable(idb1), Variable(idb2)))))
-            )
-          ))))) =>
+    coverage.markBranches(expr) match {
+      case (res, Some(ids)) if ids.size > 0 =>
         withClue(res.toString) {
-          ida1 shouldEqual a1
-          ida2 shouldEqual a2
-          Set(idb1.uniqueName, idb2.uniqueName) shouldEqual Set(b1.uniqueName, b2.uniqueName)
+          fail(s"Should have not added any ids, but got $ids")
         }
-      case _ =>
-        fail(s"$res is not of type funCall() match { case (a1, b1) => funCall() match { case (a2, b2) => (a1 + a2, b1 || b2) } }")
+      case (res, _) =>
+        res match {
+          case MatchExpr(funCall, Seq(
+                MatchCase(TuplePattern(None, Seq(WildcardPattern(Some(a1)), WildcardPattern(Some(b1)))), None,
+                  MatchExpr(funCall2, Seq(
+                    MatchCase(TuplePattern(None, Seq(WildcardPattern(Some(a2)), WildcardPattern(Some(b2)))), None,
+                      Tuple(Seq(BVPlus(Variable(ida1), Variable(ida2)), Or(Seq(Variable(idb1), Variable(idb2)))))
+                )
+              ))))) =>
+            withClue(res.toString) {
+              ida1.uniqueName shouldEqual a2.uniqueName
+              ida2.uniqueName shouldEqual a1.uniqueName
+              Set(idb1.uniqueName, idb2.uniqueName) shouldEqual Set(b1.uniqueName, b2.uniqueName)
+            }
+          case _ =>
+            fail(s"$res is not of type funCall() match { case (a1, b1) => funCall() match { case (a2, b2) => (a1 + a2, b1 || b2) } }")
+        }
     }
   }
   
@@ -167,18 +232,20 @@ class InputCoverageSuite extends LeonTestSuiteWithProgram with Matchers with Sca
     val coverage = new InputCoverage(withCoveredFun3, Set(withCoveredFun3, withCoveredFun2))
     val expr = withCoveredFun3.body.get
     
-    val (res, ids) = coverage.markBranches(expr)
-    ids should have size 2
-    res match {
-      case MatchExpr(funCall, Seq(
-            MatchCase(TuplePattern(None, Seq(WildcardPattern(Some(a)), WildcardPattern(Some(b1)))), None,
-              MatchExpr(FunctionInvocation(_, Seq(Variable(ida))), Seq(
-                MatchCase(TuplePattern(None, Seq(WildcardPattern(_), WildcardPattern(Some(b2)))), None,
-                  Tuple(Seq(p, Or(Seq(Variable(id1), Variable(id2)))))
-            )
-          ))))) if ida == a && id1 == b1 && id2 == b2 =>
-      case _ =>
-        fail(s"$res is not of type funCall() match { case (a, b1) => funCall(a) match { case (c, b2) => (c, b1 || b2) } }")
+    coverage.markBranches(expr) match {
+      case (res, None) => fail("No ids added")
+      case (res, Some(ids)) =>
+        res match {
+          case MatchExpr(funCall, Seq(
+                MatchCase(TuplePattern(None, Seq(WildcardPattern(Some(a)), WildcardPattern(Some(b1)))), None,
+                  MatchExpr(FunctionInvocation(_, Seq(Variable(ida))), Seq(
+                    MatchCase(TuplePattern(None, Seq(WildcardPattern(_), WildcardPattern(Some(b2)))), None,
+                      Tuple(Seq(p, Or(Seq(Variable(id1), Variable(id2)))))
+                )
+              ))))) if ida == a && id1 == b1 && id2 == b2 =>
+          case _ =>
+            fail(s"$res is not of type funCall() match { case (a, b1) => funCall(a) match { case (c, b2) => (c, b1 || b2) } }")
+        }
     }
   }
 }
