fe129410b2fa3a4166cc1c8c9c2cbe5f92a78d6c
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2016-04-20 16:06:43 +0200

    Add allowPartial option

--- testcases/synthesis/current/run.sh
+++ testcases/synthesis/current/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function run {
-    cmd="./leon --debug=report --timeout=60 --synthesis --solvers=smt-z3 $1"
+    cmd="./leon --debug=report --timeout=60 --synthesis --partial=off --solvers=smt-z3 $1"
     echo "Running " $cmd
     echo "------------------------------------------------------------------------------------------------------------------"
     $cmd;
