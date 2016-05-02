aec3b9359a2a71c259fcebe91c2560eacaf522d4
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2016-05-02 16:34:53 +0200

    Restore repair/runTests file

--- testcases/repair/runTests.sh
+++ testcases/repair/runTests.sh
@@ -25,15 +25,14 @@ echo "=====================================================================" >> 
 ./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=desugar  testcases/repair/Compiler/Compiler5.scala   | tee -a $fullLog
 ./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=simplify testcases/repair/Compiler/Compiler6.scala   | tee -a $fullLog
 
-
-./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=merge    testcases/repair/Heap/Heap3.scala        | tee -a $fullLog
-./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=merge    testcases/repair/Heap/Heap4.scala        | tee -a $fullLog
-./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=merge    testcases/repair/Heap/Heap5.scala        | tee -a $fullLog
-./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=merge    testcases/repair/Heap/Heap6.scala        | tee -a $fullLog
-./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=merge    testcases/repair/Heap/Heap7.scala        | tee -a $fullLog
-./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=merge    testcases/repair/Heap/Heap10.scala       | tee -a $fullLog
-./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=insert   testcases/repair/Heap/Heap8.scala        | tee -a $fullLog
-./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=makeN    testcases/repair/Heap/Heap9.scala        | tee -a $fullLog
+./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=merge    testcases/repair/Heap/Heap3.scala           | tee -a $fullLog
+./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=merge    testcases/repair/Heap/Heap4.scala           | tee -a $fullLog
+./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=merge    testcases/repair/Heap/Heap5.scala           | tee -a $fullLog
+./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=merge    testcases/repair/Heap/Heap6.scala           | tee -a $fullLog
+./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=merge    testcases/repair/Heap/Heap7.scala           | tee -a $fullLog
+./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=merge    testcases/repair/Heap/Heap10.scala          | tee -a $fullLog
+./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=insert   testcases/repair/Heap/Heap8.scala           | tee -a $fullLog
+./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=makeN    testcases/repair/Heap/Heap9.scala           | tee -a $fullLog
 
 ./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=nnf      testcases/repair/PropLogic/PropLogic1.scala | tee -a $fullLog
 ./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=nnf      testcases/repair/PropLogic/PropLogic2.scala | tee -a $fullLog
@@ -45,7 +44,6 @@ echo "=====================================================================" >> 
 ./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=++       testcases/repair/List/List2.scala           | tee -a $fullLog
 ./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=:+       testcases/repair/List/List3.scala           | tee -a $fullLog
 ./leon --debug=report --repair --timeout=30                       --functions=replace  testcases/repair/List/List5.scala           | tee -a $fullLog
-
 ./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=count    testcases/repair/List/List6.scala           | tee -a $fullLog
 ./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=find     testcases/repair/List/List7.scala           | tee -a $fullLog
 ./leon --debug=report --repair --timeout=30 --solvers=fairz3,enum --functions=find     testcases/repair/List/List8.scala           | tee -a $fullLog
