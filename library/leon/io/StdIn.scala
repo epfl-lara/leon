a4a186b9ea1b67f29f6a762a278ae6db3c57c3c9
Marco Antognini <antognini.marco@gmail.com>
2016-05-02 11:42:06 +0200

    Annotate StdIn for GenC

--- library/leon/io/StdIn.scala
+++ library/leon/io/StdIn.scala
@@ -4,16 +4,42 @@ package leon.io
 
 import leon.annotation._
 
+/*
+ * NOTEs for GenC:
+ * --------------
+ *
+ *  - `State` should be completely ignored as it is an artefact for verification
+ *    proofs. TODO It might be interesting to completely drop it from the C code
+ *    instead of aliasing it to void* and the null pointer.
+ *
+ *  - While `bool` is a standard C99 type (aliased to `_Bool`), there is no specifier
+ *    for scan operation. Additionnally, the actual size of a boolean is platform
+ *    dependent. Therefore, for simplicity, `readBoolean` is currently dropped.
+ *
+ *  - Currently, GenC doesn't support `BigInt` which implies that `readBigInt` is
+ *    dropped as well.
+ *
+ */
+
 object StdIn {
 
   @library
+  @cCode.typedef(alias = "void*")
   case class State(var seed: BigInt)
 
   @library
+  @cCode.function(code = "void* __FUNCTION__(void) { return NULL; }")
   def newState: State = State(0)
 
   @library
-  @isabelle.noBody()
+  @cCode.function(code = """
+    |int32_t __FUNCTION__(void* unused) {
+    |  int32_t x;
+    |  scanf("%"SCNd32, &x);
+    |  return x;
+    |}
+    """
+  )
   def readInt(implicit state: State): Int = {
     state.seed += 1
     nativeReadInt(state.seed)
@@ -21,12 +47,13 @@ object StdIn {
 
   @library
   @extern
+  @cCode.drop
   private def nativeReadInt(seed: BigInt): Int = {
     scala.io.StdIn.readInt
   } ensuring((x: Int) => true)
 
   @library
-  @isabelle.noBody()
+  @cCode.drop
   def readBigInt(implicit state: State): BigInt = {
     state.seed += 1
     nativeReadBigInt(state.seed)
@@ -34,12 +61,13 @@ object StdIn {
 
   @library
   @extern
+  @cCode.drop
   private def nativeReadBigInt(seed: BigInt): BigInt = {
     BigInt(scala.io.StdIn.readInt)
   } ensuring((x: BigInt) => true)
 
   @library
-  @isabelle.noBody()
+  @cCode.drop
   def readBoolean(implicit state: State): Boolean = {
     state.seed += 1
     nativeReadBoolean(state.seed)
@@ -47,6 +75,7 @@ object StdIn {
 
   @library
   @extern
+  @cCode.drop
   private def nativeReadBoolean(seed: BigInt): Boolean = {
     scala.io.StdIn.readBoolean
   } ensuring((x: Boolean) => true)
