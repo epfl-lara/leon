ba757b3dd2ad361e0728a9d9bbe5543dabeb8507
Sumith1896 <sumith1896@gmail.com>
2016-05-10 16:45:27 +0200

    Add collapsing of tuples

--- src/main/scala/leon/transformations/RunnableCodePhase.scala
+++ src/main/scala/leon/transformations/RunnableCodePhase.scala
@@ -40,17 +40,18 @@ object RunnableCodePhase extends TransformationPhase {
         FunctionInvocation(TypedFunDef(funMap(tfd.fd), tfd.tps), args)
       case Ensuring(body, pred) => removeContracts(body, fd)
       case Require(pred, body) => removeContracts(body, fd)
-      //        case Tuple(args) => {
-      //          args.head match {
-      //            case TupleSelect(v, j) if j == 1 =>
-      //              val success =  args.zipWithIndex.forall {
-      //                case (TupleSelect(u, i), index) if v == u && i == index + 1 => true
-      //                case _ => false
-      //              }
-      //              if(success) v else Tuple(args)
-      //            case e => e
-      //          }
-      //        }
+      case Tuple(args) => {
+        args.head match {
+          case TupleSelect(v, j) if j == 1 =>
+            val success =  args.zipWithIndex.forall {
+              case (TupleSelect(u, i), index) if v == u && i == index + 1 => true
+              case _ => false
+            }
+            val tup = v.getType
+            if(success && (tup.dimension == args.size)) v else Tuple(args)
+          case e => e
+        }
+      }
       case e => e
     }(ine)
 
