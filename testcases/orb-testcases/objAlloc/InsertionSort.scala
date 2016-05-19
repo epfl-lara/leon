985c475d85064403e9039f99fa306d30ec49f49d
Sumith1896 <sumith1896@gmail.com>
2016-05-19 15:33:34 +0200

    Update the InsertionSort example

--- testcases/orb-testcases/objAlloc/InsertionSort.scala
+++ testcases/orb-testcases/objAlloc/InsertionSort.scala
@@ -15,14 +15,14 @@ object InsertionSort {
 
   def sortedIns(e: BigInt, l: List): List = {
     l match {
-      case Cons(x,xs) => if (x <= e) Cons(x,sortedIns(e, xs)) else Cons(e, l)
+      case Cons(x,xs) => if (x < e) Cons(x,sortedIns(e, xs)) else Cons(e, l)
       case _ => Cons(e,Nil())
     }
-  } ensuring(res => size(res) == size(l) + 1 && obj <= ?)
+  } ensuring(res => size(res) == size(l) + 1 && obj <= ? * size(res) + ?)
 
-  def sort(l: List): List = (l match {
-    case Cons(x,xs) => sortedIns(x, sort(xs))
-    case _ => Nil()
-  }) ensuring(res => size(res) == size(l) && obj <= ?)
+    def sort(l: List): List = (l match {
+      case Cons(x,xs) => sortedIns(x, sort(xs))
+      case _ => Nil()
+    }) ensuring(res => size(res) == size(l) && obj <= ? *(size(res)*size(res)) + ? * size(res) + ?)
 }
 
