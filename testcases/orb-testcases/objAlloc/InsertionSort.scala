8d08a5f31855b7612d75974129ae226d2bbd0c3e
Sumith1896 <sumith1896@gmail.com>
2016-05-19 14:28:12 +0200

    Unable to obj for size in InsertionSort

--- testcases/orb-testcases/objAlloc/InsertionSort.scala
+++ testcases/orb-testcases/objAlloc/InsertionSort.scala
@@ -1,28 +1,28 @@
-  import leon.invariant._
-  import leon.instrumentation._
+import leon.invariant._
+import leon.instrumentation._
 
-  object InsertionSort {
-    sealed abstract class List
-    case class Cons(head: BigInt, tail:List) extends List
-    case class Nil() extends List
+object InsertionSort {
+  sealed abstract class List
+  case class Cons(head: BigInt, tail:List) extends List
+  case class Nil() extends List
 
-    def size(l : List): BigInt = {
-      l match {
-        case Cons(_, xs) => 1 + size(xs)
-        case _ => 0
-      }
-    } ensuring(res => res >= 0 && obj <= ?)
-
-    def sortedIns(e: BigInt, l: List): List = {
-      l match {
-        case Cons(x,xs) => if (x <= e) Cons(x,sortedIns(e, xs)) else Cons(e, l)
-        case _ => Cons(e,Nil())
-      }
-    } ensuring(res => size(res) == size(l) + 1 && obj <= ?)
-
-    def sort(l: List): List = (l match {
-      case Cons(x,xs) => sortedIns(x, sort(xs))
-      case _ => Nil()
-    }) ensuring(res => size(res) == size(l) && obj <= ?)
+  def size(l : List): BigInt = {
+    l match {
+      case Cons(_, xs) => 1 + size(xs)
+      case _ => 0
+    }
   }
+
+  def sortedIns(e: BigInt, l: List): List = {
+    l match {
+      case Cons(x,xs) => if (x <= e) Cons(x,sortedIns(e, xs)) else Cons(e, l)
+      case _ => Cons(e,Nil())
+    }
+  } ensuring(res => size(res) == size(l) + 1 && obj <= ?)
+
+  def sort(l: List): List = (l match {
+    case Cons(x,xs) => sortedIns(x, sort(xs))
+    case _ => Nil()
+  }) ensuring(res => size(res) == size(l) && obj <= ?)
+}
 
