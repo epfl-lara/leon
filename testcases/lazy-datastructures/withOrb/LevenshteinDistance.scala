edb2c875cfcfd37d2b0d97f1689465a9611da17b
Sumith1896 <sumith1896@gmail.com>
2016-05-24 17:57:53 +0200

    Implemented LevenshteinDistance

--- testcases/lazy-datastructures/withOrb/LevenshteinDistance.scala
+++ testcases/lazy-datastructures/withOrb/LevenshteinDistance.scala
@@ -8,7 +8,7 @@ import instrumentation._
 import invariant._
 import collection._
 
-object LongestCommonSubsequence {
+object LevenshteinDistance {
 
   @ignore
   var xstring = Array[BigInt]()
@@ -22,7 +22,7 @@ object LongestCommonSubsequence {
   // deps and it's lemmas
   def deps(i: BigInt, j: BigInt): Boolean = {
     require(i >= 0 && j >= 0)
-    lcs(i, j).cached &&
+    levDist(i, j).cached &&
       (if (i <= 0 && j <= 0) true
       else if (i <= 0) deps(i, j - 1)
       else if (j <= 0) deps(i - 1, j)
@@ -45,25 +45,26 @@ object LongestCommonSubsequence {
   @invstate
   @memoize
   @invisibleBody
-  def lcs(i: BigInt, j: BigInt): BigInt = {
+  def levDist(i: BigInt, j: BigInt): BigInt = {
     require((i >=0 && j >= 0) && (i == 0 || deps(i - 1, j)) && (j == 0 || deps(i, j-1)))
-    if (i == 0 || j == 0) BigInt(0)
+    if (i == 0) j
+    else if(j == 0) i
     else {
       val (xi, yj) = lookup(i, j)
-      if (xi == yj)
-        lcs(i - 1, j - 1) + 1
-      else {
-        val s1 = lcs(i - 1, j)
-        val s2 = lcs(i, j - 1)
+      val a1 = if (xi == yj) levDist(i - 1, j - 1) else levDist(i - 1, j - 1) + 1
+      val a2 = {
+        val s1 = levDist(i - 1, j)
+        val s2 = levDist(i, j - 1)
         if (s1 >= s2) s1 else s2
       }
+      if (a1 >= a2) a1 else a2
     }
   } ensuring (_ => time <= ?)
 
   @invisibleBody
   def invoke(i: BigInt, j: BigInt, n: BigInt) = {
     require((i >=0 && j >= 0 && n >= j) && (i == 0 || deps(i - 1, j)) && (j == 0 || deps(i, j-1)))
-    lcs(i, j)
+    levDist(i, j)
   } ensuring (res => {
     val in = inState[BigInt]
     val out = outState[BigInt]
@@ -74,13 +75,8 @@ object LongestCommonSubsequence {
   })
 
   /**
-   * Given a m x n DP problem, the following function solves the subproblems by traversing the problem space
-   * from right to left, and bottom to top.
-   * @param m - number of rows remaining
-   * @param n - max. number of columns
-   * @param j - number of columns remaining (initially set to 1)
-   * @result returns a list of solutions for each sub-problem (the size of the resulting list will be quadratic)
-   */
+  * Same as LCS, look at that file for more insights
+  */
   def bottomup(m: BigInt, j: BigInt, n: BigInt): List[BigInt] = {
     require(0 <= m && 0 <= j && j <= n)
     if (m == 0 && j == 0) {
@@ -106,16 +102,8 @@ object LongestCommonSubsequence {
       depsLem(m, 0, m, j)
   }
 
-  def lcsSols(m: BigInt, n: BigInt): List[BigInt] = {
+  def levDistSols(m: BigInt, n: BigInt): List[BigInt] = {
     require(0 <= m && 0 <= n)
     bottomup(m, n, n)
   } ensuring(_ => time <= ? * (m * n)  + ? * m + ? * n + ?)
-
-  /*@ignore
-  def main(args: Array[String]) {
-    import scala.util.Random
-    // TODO: Use randomised test cases
-    val common = lcsSol(string1, string2)
-    println("Common substring of " + string1.toString + " and " + string2.toString + " is " + common.toString)
-  }*/
 }
