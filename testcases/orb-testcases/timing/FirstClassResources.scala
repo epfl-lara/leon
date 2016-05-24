ad8b56e8eebdbb7c72f503fd6be8ea42cd5e268f
ravi <ravi.kandhadai@epfl.ch>
2016-05-25 00:06:43 +0200

    Adding a testcase with first class suspensions

---
+++ testcases/orb-testcases/timing/FirstClassResources.scala
@@ -0,0 +1,33 @@
+import leon.invariant._
+import leon.instrumentation._
+
+object TreeOperations {
+
+  sealed abstract class Tree
+  case class Node(left: Tree, value: BigInt, right: Tree) extends Tree
+  case class Leaf() extends Tree
+
+  def size(t: Tree): BigInt = {
+    t match {
+      case Leaf() => 0
+      case Node(l, x, r) => {
+        size(l) + size(r) + 1
+      }
+    }
+  }
+
+  def height(t: Tree): BigInt = {
+    t match {
+      case Leaf() => 0
+      case Node(l, x, r) => {
+        val hl = height(l)
+        val hr = height(r)
+        if (hl > hr) hl + 1 else hr + 1
+      }
+    }
+  }
+
+  def timeOfSizeHeight(t: Tree): Boolean = {
+    time(size(t)) <= time(height(t))
+  }
+}
