87b22b3821984ff4572ef8d2f3b3361a77ec5ab8
Ravi <ravi.kandhadai@epfl.ch>
2016-06-04 13:24:14 +0200

    Updating stream library, and refactoring traceInduct to be incorportated into Orb later if necessary

--- testcases/lazy-datastructures/withOrb/Haskell-Library/LibraryClient.scala
+++ testcases/lazy-datastructures/withOrb/Haskell-Library/LibraryClient.scala
@@ -1,4 +1,4 @@
-package stream
+package haskell
 
 import leon._
 import lang._
@@ -11,67 +11,153 @@ import invariant._
 import StreamLibrary._
 
 object NatStream {
-
-  def nthElemInNatsFromM(n: BigInt, M: BigInt) = {
+  def mapClient(n: BigInt) = {
     require(n >= 0)
-    getnthElem(n, natsFromn(M))
+    nthElemAfterMap(n, map(constFun1, natsFromn(0)))
+  }
+
+  @inline
+  def mapStream(s: LList) = {
+    s match {
+      case SCons(_, tfun) => tfun fmatch[BigInt => BigInt, LList, Boolean] {
+          case (f, n) if tfun.is(() => mapSusp(f, n)) => true
+          case _ => false
+        }  
+      case _              => true
+    }
+  }
+  
+  def nthElemAfterMap(n: BigInt, s: LList): BigInt = {
+    require(n >= 0 && mapStream(s))
+    s match {
+      case SNil() => BigInt(0)
+      case s @ SCons(x, _) =>
+        if (n == 0) x
+        else nthElemAfterMap(n - 1, s.tailOrNil)
+    }
+  } ensuring (_ => time <= ? * n + ?) // Orb result: ??
+    
+  @inline
+  def repeatStream(s: LList) = {
+    s match {
+      case SCons(_, tfun) => tfun fmatch[BigInt, Boolean] {
+          case n if tfun.is(() => repeat(n)) => true
+          case _ => false
+        }  
+      case _              => true
+    }
+  }
+  
+  def nthElemInRepeat(n: BigInt, s: LList): BigInt = {
+    require(n >= 0 && repeatStream(s))
+    s match {
+      case SNil() => BigInt(0)
+      case s @ SCons(x, _) =>
+        if (n == 0) x
+        else nthElemInRepeat(n - 1, s.tailOrNil)
+    }
+  } ensuring (_ => time <= ? * n + ?) // Orb result: ??
+  
+  def takeWhileClient(n: BigInt) = {
+    require(n >= 0)
+    nthElemAfterTakeWhile(n,takeWhile(constFun2, natsFromn(0)))
+  } 
+  
+  @inline
+  def takeWhileStream(s: LList) = {
+    s match {
+      case SCons(_, tfun) => tfun fmatch[BigInt => Boolean, LList, Boolean] {
+          case (p, l) if tfun.is(() => takeWhileSusp(p, l)) => true
+          case _ => false
+        }  
+      case _              => true
+    }
+  }
+
+  def nthElemAfterTakeWhile(n: BigInt, s: LList): BigInt  = {
+    require(n >= 0 && takeWhileStream(s))
+    s match {
+      case SNil() => BigInt(0)
+      case s @ SCons(x, _) =>
+        if (n == 0) x
+        else nthElemAfterTakeWhile(n - 1, s.tailOrNil)
+    }
   } ensuring (_ => time <= ? * n + ?) // Orb result: ??
 
-  def nthElemInRepeat(n: BigInt, m: BigInt) = {
+  def scanClient(n: BigInt) = {
     require(n >= 0)
-    getnthElem(n, repeat(m))
+    nthElemAfterScan(n, scan(constFun3, BigInt(0), natsFromn(0)))
+  }
+  
+  @inline
+  def scanStream(s: LList) = {
+    s match {
+      case SCons(_, tfun) => tfun fmatch[(BigInt, BigInt) => BigInt, BigInt, LList, Boolean] {
+          case (acc, z, l) if tfun.is(() => scanSusp(acc, z, l)) => true
+          case _ => false
+        }  
+      case _              => true
+    }
+  }
+  
+  def nthElemAfterScan(n: BigInt, s: LList): BigInt  = {
+    require(n >= 0 && scanStream(s))
+    s match {
+      case SNil() => BigInt(0)
+      case s @ SCons(x, _) =>
+        if (n == 0) x
+        else nthElemAfterScan(n - 1, s.tailOrNil)
+    }
+  } ensuring (_ => time <= ? * n + ?) // Orb result: ??
+  
+  def unfoldClient(n: BigInt, c: BigInt) = {
+    require(n >= 0)
+    nthElemAfterUnfold(n, unfold(constFun4, c))
   } ensuring (_ => time <= ? * n + ?) // Orb result: ??
 
-  def nthElemInCycle(n: BigInt, l: List[BigInt]) = {
-    require(n >= 0)
-    getnthElem(n, cycle(l))
+  @inline
+  def unfoldStream(s: LList) = {
+    s match {
+      case SCons(_, tfun) => tfun fmatch[BigInt => (BigInt, BigInt), BigInt, Boolean] {
+          case (f, n) if tfun.is(() => unfold(f, n)) => true
+          case _ => false
+        }  
+      case _              => true
+    }
+  }
+  def nthElemAfterUnfold(n: BigInt, s: LList): BigInt  = {
+    require(n >= 0 && unfoldStream(s))
+    s match {
+      case SNil() => BigInt(0)
+      case s @ SCons(x, _) =>
+        if (n == 0) x
+        else nthElemAfterUnfold(n - 1, s.tailOrNil)
+    }
   } ensuring (_ => time <= ? * n + ?) // Orb result: ??
 
-  @ignore
-  val array = Array[BigInt]()
-  @extern
-  def f(n: BigInt): BigInt = {
-    array(0)
-  } ensuring (_ => time <= 1)
-
-  def nthElemAfterMap(n: BigInt) = {
+  def zipWithClient(n: BigInt) = {
     require(n >= 0)
-    getnthElem(n, map(f, natsFromn(0)))
+    nthElemAfterZipWith(n, zipWith(constFun3, natsFromn(0), natsFromn(0)))
+  }
+  
+  @inline
+  def zipWithStream(s: LList) = {
+    s match {
+      case SCons(_, tfun) => tfun fmatch[(BigInt, BigInt) => BigInt, LList, LList, Boolean] {
+          case (f, l1, l2) if tfun.is(() => zipWithSusp(f, l1, l2)) => true
+          case _ => false
+        }  
+      case _              => true
+    }
+  }
+  
+  def nthElemAfterZipWith(n: BigInt, s: LList): BigInt  = {
+    require(n >= 0 && zipWithStream(s))
+    s match {
+      case SNil() => BigInt(0)
+      case s @ SCons(x, _) =>
+        if (n == 0) x
+        else nthElemAfterZipWith(n - 1, s.tailOrNil)
+    }
   } ensuring (_ => time <= ? * n + ?) // Orb result: ??
-
-  @extern
-  def p(n: BigInt): Boolean = {
-    array(0) == 0
-  } ensuring (_ => time <= 1)
-
-  def nthElemAfterTakeWhile(n: BigInt) = {
-    require(n >= 0)
-    getnthElem(n, takeWhile(p, natsFromn(0)))
-  } ensuring (_ => time <= ? * n + ?) // Orb result: ??
-
-  @extern
-  def acc(n: BigInt, m: BigInt): BigInt = {
-    array(0)
-  } ensuring (_ => time <= 1)
-
-  def nthElemAfterScan(n: BigInt, z: BigInt) = {
-    require(n >= 0)
-    getnthElem(n, scan(acc, z, natsFromn(0)))
-  } ensuring (_ => time <= ? * n + ?) // Orb result: ??
-
-  @extern
-  def tupFunc(n: BigInt): (BigInt, BigInt) = {
-    (array(0), array(0))
-  } ensuring (_ => time <= 1)
-
-  def nthElemAfterUnfold(n: BigInt, c: BigInt) = {
-    require(n >= 0)
-    getnthElem(n, unfold(tupFunc, c))
-  } ensuring (_ => time <= ? * n + ?) // Orb result: ??
-
-  def nthElemAfterZipWith(n: BigInt) = {
-    require(n >= 0)
-    getnthElem(n, zipWith(acc, natsFromn(0), natsFromn(0)))
-  } ensuring (_ => time <= ? * n + ?) // Orb result: ??
-
 }
