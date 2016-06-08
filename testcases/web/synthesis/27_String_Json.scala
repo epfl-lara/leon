bd707a0659138b043cd9fed471cdf0af3fe11c12
Mikaël Mayer <mikael.mayer@epfl.ch>
2016-06-08 17:37:12 +0200

    Renamed 26_Modular_Render.scala Added 27_String_Json.scala

---
+++ testcases/web/synthesis/27_String_Json.scala
@@ -0,0 +1,30 @@
+/** 
+  * Name:     JsonRender.scala
+  * Creation: 25.11.2015
+  * Author:   Mikael Mayer (mikael.mayer@epfl.ch)
+  * Comments: Json specifications
+  */
+
+import leon.lang._
+import leon.annotation._
+import leon.collection._
+import leon.collection.ListOps._
+import leon.lang.synthesis._
+
+object JsonRender {
+  abstract class JSON
+  abstract class JCompositeValue extends JSON
+  abstract class JFinalValue extends JSON
+  case class JDict(values: List[(String, JSON)]) extends JCompositeValue
+  case class JArray(values: List[JSON]) extends JCompositeValue
+  case class JString(value: String) extends JFinalValue
+  case class JBoolean(value: Boolean) extends JFinalValue
+  case class JInt(value: Int) extends JFinalValue
+  
+  def JStringToString(j: JString) = "\"" + StrOps.escape(j.value) + "\""
+  
+  /** Synthesize this function by example to have a JSON serializer. */
+  def json_render(j: JSON): String = {
+    ???[String] ask j
+  }
+}
