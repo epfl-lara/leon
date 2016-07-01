d288043d48151f816a738ba7f0145cfb6cf21b65
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2016-07-01 17:21:32 +0200

    Improve repair testing file

--- testcases/synt2016/repair/runTests.sh
+++ testcases/synt2016/repair/runTests.sh
@@ -16,46 +16,53 @@ echo "#           Category,                 File,             function, p.S, fuS
 
 #All benchmarks:
 
+function run {
+  cmd="./leon --debug=report --timeout=60 --repair --introreccalls=off --solvers=fairz3,enum --functions=$1 $2"
+  echo "Running " $cmd
+  echo "------------------------------------------------------------------------------------------------------------------"
+  $cmd;
+}
+
 echo "=====================================================================" >> repair-report.txt
 
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=desugar  testcases/synt2016/repair/Compiler/Compiler1.scala   | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=desugar  testcases/synt2016/repair/Compiler/Compiler2.scala   | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=desugar  testcases/synt2016/repair/Compiler/Compiler3.scala   | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=desugar  testcases/synt2016/repair/Compiler/Compiler4.scala   | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=desugar  testcases/synt2016/repair/Compiler/Compiler5.scala   | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=simplify testcases/synt2016/repair/Compiler/Compiler6.scala   | tee -a $fullLog
+run desugar  testcases/synt2016/repair/Compiler/Compiler1.scala   
+run desugar  testcases/synt2016/repair/Compiler/Compiler2.scala   
+run desugar  testcases/synt2016/repair/Compiler/Compiler3.scala   
+run desugar  testcases/synt2016/repair/Compiler/Compiler4.scala   
+run desugar  testcases/synt2016/repair/Compiler/Compiler5.scala   
+run simplify testcases/synt2016/repair/Compiler/Compiler6.scala   
 
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=merge    testcases/synt2016/repair/Heap/Heap3.scala           | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=merge    testcases/synt2016/repair/Heap/Heap4.scala           | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=merge    testcases/synt2016/repair/Heap/Heap5.scala           | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=merge    testcases/synt2016/repair/Heap/Heap6.scala           | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=merge    testcases/synt2016/repair/Heap/Heap7.scala           | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=merge    testcases/synt2016/repair/Heap/Heap10.scala          | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=insert   testcases/synt2016/repair/Heap/Heap8.scala           | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=makeN    testcases/synt2016/repair/Heap/Heap9.scala           | tee -a $fullLog
+run merge    testcases/synt2016/repair/Heap/Heap3.scala           
+run merge    testcases/synt2016/repair/Heap/Heap4.scala           
+run merge    testcases/synt2016/repair/Heap/Heap5.scala           
+run merge    testcases/synt2016/repair/Heap/Heap6.scala           
+run merge    testcases/synt2016/repair/Heap/Heap7.scala           
+run merge    testcases/synt2016/repair/Heap/Heap10.scala          
+run insert   testcases/synt2016/repair/Heap/Heap8.scala           
+run makeN    testcases/synt2016/repair/Heap/Heap9.scala           
 
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=pad      testcases/synt2016/repair/List/List1.scala           | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=++       testcases/synt2016/repair/List/List2.scala           | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=:+       testcases/synt2016/repair/List/List3.scala           | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60                       --functions=replace  testcases/synt2016/repair/List/List5.scala           | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=count    testcases/synt2016/repair/List/List6.scala           | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=find     testcases/synt2016/repair/List/List7.scala           | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=find     testcases/synt2016/repair/List/List8.scala           | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60                       --functions=find     testcases/synt2016/repair/List/List9.scala           | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=size     testcases/synt2016/repair/List/List10.scala          | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=sum      testcases/synt2016/repair/List/List11.scala          | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=-        testcases/synt2016/repair/List/List12.scala          | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60                       --functions=drop     testcases/synt2016/repair/List/List4.scala           | tee -a $fullLog
-#./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=drop     testcases/synt2016/repair/List/List13.scala          | tee -a $fullLog
+run pad      testcases/synt2016/repair/List/List1.scala           
+run ++       testcases/synt2016/repair/List/List2.scala           
+run :+       testcases/synt2016/repair/List/List3.scala           
+run replace  testcases/synt2016/repair/List/List5.scala           
+run count    testcases/synt2016/repair/List/List6.scala           
+run find     testcases/synt2016/repair/List/List7.scala           
+run find     testcases/synt2016/repair/List/List8.scala           
+run find     testcases/synt2016/repair/List/List9.scala           
+run size     testcases/synt2016/repair/List/List10.scala          
+run sum      testcases/synt2016/repair/List/List11.scala          
+run -        testcases/synt2016/repair/List/List12.scala          
+run drop     testcases/synt2016/repair/List/List4.scala           
+run drop     testcases/synt2016/repair/List/List13.scala          
 
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=power    testcases/synt2016/repair/Numerical/Numerical1.scala | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=moddiv   testcases/synt2016/repair/Numerical/Numerical3.scala | tee -a $fullLog
+run power    testcases/synt2016/repair/Numerical/Numerical1.scala 
+run moddiv   testcases/synt2016/repair/Numerical/Numerical3.scala 
 
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=split    testcases/synt2016/repair/MergeSort/MergeSort1.scala | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=merge    testcases/synt2016/repair/MergeSort/MergeSort2.scala | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=merge    testcases/synt2016/repair/MergeSort/MergeSort3.scala | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=merge    testcases/synt2016/repair/MergeSort/MergeSort4.scala | tee -a $fullLog
-./leon --debug=report --introreccalls=no --repair --timeout=60 --solvers=fairz3,enum --functions=merge    testcases/synt2016/repair/MergeSort/MergeSort5.scala | tee -a $fullLog
+run split    testcases/synt2016/repair/MergeSort/MergeSort1.scala 
+run merge    testcases/synt2016/repair/MergeSort/MergeSort2.scala 
+run merge    testcases/synt2016/repair/MergeSort/MergeSort3.scala 
+run merge    testcases/synt2016/repair/MergeSort/MergeSort4.scala 
+run merge    testcases/synt2016/repair/MergeSort/MergeSort5.scala 
 
 # Average results
 #cat $log >> $summaryLog
