0fc45a4c2f7caff3cc4a2d1b0ee9a0bbb4b48cea
Romain Edelmann <romain.edelmann@gmail.com>
2016-07-10 09:10:23 +0200

    Used debug instead of info.

--- src/main/scala/leon/evaluators/ProofEvaluator.scala
+++ src/main/scala/leon/evaluators/ProofEvaluator.scala
@@ -10,6 +10,9 @@ import purescala.Types._
 import leon.verification.VerificationContext
 import leon.verification.theorem._
 
+import leon.utils.DebugSection
+import leon.utils.DebugSectionProof
+
 /** Evaluator of proof functions.
   *
   * Adds support for all "magic" functions defined in the leon.theorem library.
@@ -20,20 +23,21 @@ class ProofEvaluator(ctx: VerificationContext, prog: Program)
   // Set of verification conditions generated during evaluations.
   private var vcs: Seq[Expr] = Seq()
 
+  private implicit val debugSection: DebugSection = DebugSectionProof
   private val library = new Library(prog)
   private val encoder = new ExprEncoder(ctx)
 
   override protected[evaluators] def e(expr: Expr)(implicit rctx: RC, gctx: GC): Expr = expr match {
     // Invocation of prove.
     case FunctionInvocation(TypedFunDef(fd, Seq()), Seq(arg)) if (fd == library.prove.get) => {
-      ctx.reporter.info("Called solve.")
+      ctx.reporter.debug("Called solve.")
       val evaluatedArg = e(arg).setPos(expr)
       vcs = vcs :+ evaluatedArg
       super.e(FunctionInvocation(TypedFunDef(fd, Seq()), Seq(evaluatedArg)))
     }
     // Invocation of fresh.
     case FunctionInvocation(TypedFunDef(fd, Seq()), Seq(arg)) if (fd == library.fresh.get) => {
-      ctx.reporter.info("Called fresh.")
+      ctx.reporter.debug("Called fresh.")
       val StringLiteral(name) = e(arg)
       val freshName = FreshIdentifier(name, Untyped, true).uniqueName
       encoder.caseClass(library.Identifier, StringLiteral(freshName))
