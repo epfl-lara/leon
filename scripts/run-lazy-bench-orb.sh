d841c5e666c40b6d00ab2c1abd877466ef4a76b8
Ravi <ravi.kandhadai@epfl.ch>
2016-07-22 16:49:41 +0530

    Adding scripts for running benchmarks

--- scripts/run-lazy-bench-orb.sh
+++ scripts/run-lazy-bench-orb.sh
@@ -1,19 +1,20 @@
-runlim -t 1800 ./leon --mem --benchmark ./testcases/lazy-datastructures/withOrb/SortingnConcat.scala | tee SortingnConcat.out # runs in a few seconds
-runlim -t 1800 ./leon --mem --benchmark --unrollfactor=2 ./testcases/lazy-datastructures/withOrb/RealTimeQueue.scala | tee  RealTimeQueue.out # take ~1min
-runlim -t 1800 ./leon --mem --benchmark --unrollfactor=4 ./testcases/lazy-datastructures/withOrb/CyclicFibStream.scala  | tee CyclicFibStream.out # a few secs
-runlim -t 1800 ./leon --mem --benchmark --unrollfactor=4 ./testcases/lazy-datastructures/withOrb/PreciseBottomUpMegeSort.scala | tee PreciseBottomUpMegeSort.out
-runlim -t 1800 ./leon --mem --benchmark --unrollfactor=5 ./testcases/lazy-datastructures/withOrb/LazyNumericalRep.scala  | tee LazyNumericalRep.out # in a few secs
-runlim -t 1800 ./leon --mem --benchmark --unrollfactor=4 ./testcases/lazy-datastructures/withOrb/Deque.scala | tee Deque.out # takes ~ 2min
-runlim -t 1800 ./leon --mem --benchmark ./testcases/lazy-datastructures/withOrb/Knapsack.scala  | tee Knapsack.out # a few seconds
-runlim -t 1800 ./leon --mem --benchmark ./testcases/lazy-datastructures/withOrb/WeightedScheduling.scala | tee Sched.out # a few seconds
-runlim -t 1800 ./leon --mem --benchmark --unrollfactor=2 ./testcases/lazy-datastructures/withOrb/LongestCommonSubsequence.scala  | tee LongestCommonSubsequence.out # a few seconds
-runlim -t 1800 ./leon --mem --benchmark --unrollfactor=2 ./testcases/lazy-datastructures/withOrb/LevenshteinDistance.scala  | tee LevenshteinDistance.out # a few seconds
-runlim -t 1800 ./leon --mem --benchmark --unrollfactor=4 ./testcases/lazy-datastructures/withOrb/PackratParsing.scala  | tee PackratParsing.out # ~ 1min
-runlim -t 1800 ./leon --mem --benchmark ./testcases/lazy-datastructures/withOrb/Haskell-Library/StreamLibrary.scala ./testcases/lazy-datastructures/withOrb/Haskell-Library/StreamClient.scala | tee StreamLibrary.out
-runlim -t 1800 ./leon --mem --benchmark --unrollfactor=5 ./testcases/lazy-datastructures/conc/ConcTrees.scala ./testcases/lazy-datastructures/conc/Conqueue.scala | tee Conqueue.out
-runlim -t 1800 ./leon --mem --benchmark --unrollfactor=4 --vcTimeout=35 ./testcases/lazy-datastructures/withOrb/CyclicHammingStream.scala  | tee CyclicHammingStream.out # takes ~ 621s (somehow correct VCs take less time but still requires large timeout)
-runlim -t 1800 ./leon --mem --benchmark --unrollfactor=3 --vcTimeout=35 ./testcases/lazy-datastructures/withOrb/BottomUpMegeSort.scala | tee BottomUpMegeSort.out	# works in ~ 5 min, but a time bound of a function is manually provided
-runlim -t 1800 ./leon --mem --benchmark --unrollfactor=3 ./testcases/lazy-datastructures/withOrb/HammingMemoized.scala | tee HammingMemoized.out   # may take up to 1000s need to optimize
+./leon --mem --benchmark --timeout=1800 ./testcases/lazy-datastructures/withOrb/SortingnConcat.scala | tee SortingnConcat.out # runs in a few seconds
+./leon --mem --benchmark --unrollfactor=2  --timeout=1800 ./testcases/lazy-datastructures/withOrb/RealTimeQueue.scala | tee  RealTimeQueue.out # take ~1min
+./leon --mem --benchmark --unrollfactor=4  --timeout=1800 ./testcases/lazy-datastructures/withOrb/CyclicFibStream.scala  | tee CyclicFibStream.out # a few secs
+#./leon --mem --benchmark --unrollfactor=4  --timeout=1800 ./testcases/lazy-datastructures/withOrb/PreciseBottomUpMegeSort.scala | tee PreciseBottomUpMegeSort.out
+./leon --mem --benchmark --unrollfactor=5  --timeout=1800 ./testcases/lazy-datastructures/withOrb/LazyNumericalRep.scala  | tee LazyNumericalRep.out # in a few secs
+./leon --mem --benchmark --unrollfactor=4   --timeout=1800 ./testcases/lazy-datastructures/withOrb/Deque.scala | tee Deque.out # takes ~ 2min
+./leon --mem --benchmark --unrollfactor=4  --timeout=1800 ./testcases/lazy-datastructures/withOrb/CyclicHammingStream.scala  | tee CyclicHammingStream.out # takes about 2 min
+./leon --mem --benchmark  --timeout=1800 ./testcases/lazy-datastructures/withOrb/Knapsack.scala  | tee Knapsack.out # a few seconds
+./leon --mem --benchmark  --timeout=1800 ./testcases/lazy-datastructures/withOrb/WeightedScheduling.scala | tee Sched.out # a few seconds
+./leon --mem --benchmark --unrollfactor=2  --timeout=1800 ./testcases/lazy-datastructures/withOrb/LongestCommonSubsequence.scala  | tee LongestCommonSubsequence.out # a few seconds
+./leon --mem --benchmark --unrollfactor=2  --timeout=1800 ./testcases/lazy-datastructures/withOrb/LevenshteinDistance.scala  | tee LevenshteinDistance.out # a few seconds
+./leon --mem --benchmark --unrollfactor=4  --timeout=1800 ./testcases/lazy-datastructures/withOrb/PackratParsing.scala  | tee PackratParsing.out # ~ 1min
+./leon --mem --benchmark  --timeout=1800 ./testcases/lazy-datastructures/withOrb/Haskell-Library/StreamLibrary.scala ./testcases/lazy-datastructures/withOrb/Haskell-Library/StreamClient.scala | tee StreamLibrary.out
+./leon --mem --benchmark --unrollfactor=5  --timeout=1800 ./testcases/lazy-datastructures/conc/ConcTrees.scala ./testcases/lazy-datastructures/conc/Conqueue.scala | tee Conqueue.out
+./leon --mem --benchmark --unrollfactor=3  --timeout=1800 ./testcases/lazy-datastructures/withOrb/HammingMemoized.scala | tee HammingMemoized.out   # may take up to 1000s need to optimize
+#./leon --mem --benchmark --unrollfactor=3 --vcTimeout=35 ./testcases/lazy-datastructures/withOrb/BottomUpMegeSort.scala | tee BottomUpMegeSort.out	# works in ~ 5 min, but a time bound of a function is manually provided
+
 
 
 
---
+++ scripts/run-orb-benchmarks.sh
@@ -0,0 +1,5 @@
+./leon --inferInv --benchmark --minbounds=0 --vcTimeout=3 --timeout=1800  ./testcases/orb-testcases/timing/BinomialHeap.scala # < 10s
+./leon --inferInv --benchmark --minbounds=0 --vcTimeout=3 --timeout=1800  ./testcases/orb-testcases/timing/LeftistHeap.scala # < 20s
+./leon --inferInv --benchmark --minbounds=0 --vcTimeout=3 --timeout=1800  ./testcases/orb-testcases/timing/AVLTree.scala # < 30s
+./leon --inferInv --benchmark --minbounds=0 --vcTimeout=3 --timeout=1800  ./testcases/orb-testcases/timing/RedBlackTree.scala # < 40s
+./leon --inferInv --benchmark --minbounds=0 --disableInfer --solvers=orb-smt-cvc4 --timeout=1800  ./testcases/orb-testcases/timing/ConcTrees.scala # < 100s
