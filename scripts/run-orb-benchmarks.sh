b2b49ae2c7b66913e424f85ca0b46a55e1b4a786
Ravi <ravi.kandhadai@epfl.ch>
2016-07-23 01:04:22 +0530

    Minor changes to scripts

--- scripts/run-orb-benchmarks.sh
+++ scripts/run-orb-benchmarks.sh
@@ -1,4 +1,4 @@
-./leon --inferInv --benchmark --minbounds=0 --vcTimeout=3 --timeout=1800  ./testcases/orb-testcases/timing/BinomialHeap.scala # < 10s
-./leon --inferInv --benchmark --minbounds=0 --vcTimeout=3 --timeout=1800  ./testcases/orb-testcases/timing/LeftistHeap.scala # < 20s
-./leon --inferInv --benchmark --minbounds=0 --vcTimeout=3 --timeout=1800  ./testcases/orb-testcases/timing/AVLTree.scala # < 30s
-./leon --inferInv --benchmark --minbounds=0 --vcTimeout=3 --timeout=1800  ./testcases/orb-testcases/timing/RedBlackTree.scala # < 40s
+./leon --inferInv --benchmark --minbounds=0 --vcTimeout=3 --timeout=1800  ./testcases/orb-testcases/timing/BinomialHeap.scala | tee BinomialHeap.out # < 10s
+./leon --inferInv --benchmark --minbounds=0 --vcTimeout=3 --timeout=1800  ./testcases/orb-testcases/timing/LeftistHeap.scala  | tee LeftistHeap.out # < 20s
+./leon --inferInv --benchmark --minbounds=0 --vcTimeout=3 --timeout=1800  ./testcases/orb-testcases/timing/AVLTree.scala | tee AVLTree.out # < 30s
+./leon --inferInv --benchmark --minbounds=0 --vcTimeout=3 --timeout=1800  ./testcases/orb-testcases/timing/RedBlackTree.scala  | tee RedBlackTree.out # < 40s
