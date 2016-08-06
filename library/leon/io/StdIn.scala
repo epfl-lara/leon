b1769443c5ee9cba90b739396b84adbf585bdd3b
Marco Antognini <antognini.marco@gmail.com>
2016-08-06 10:56:46 +0200

    Fix Leon implementation of leon.io.StdIn.nativeReadInt

--- library/leon/io/StdIn.scala
+++ library/leon/io/StdIn.scala
@@ -32,6 +32,11 @@ import leon.annotation._
 
 object StdIn {
 
+  /**
+   * Reads the next signed decimal integer
+   *
+   * TODO to support other integer bases, one has to use SCNi32 in C.
+   */
   @library
   @cCode.function(code = """
     |int32_t __FUNCTION__(void* unused) {
@@ -50,7 +55,92 @@ object StdIn {
   @extern
   @cCode.drop
   private def nativeReadInt(seed: BigInt): Int = {
-    scala.io.StdIn.readInt
+    /* WARNING This code is singificantly a duplicate of leon.io.FileInputStream.nativeReadInt
+     *         because there's no clean way to refactor this in Leon's library.
+     *
+     * This implementation mimic `scanf("%d")` for 32-bit integer.
+     *
+     * NOTE
+     *  - The `%d` flag is for signed decimal integers;
+     *  - The format of the number is the same as expected by strtol() with the value 10 for the base argument;
+     *  - This format corresponds to the following regex:
+     *
+     *              \s*[+-]?\d+
+     *
+     *    where
+     *      - `\s` match any character c such that `isspace(c)` holds;
+     *      - a negative number (indicated by a leading `-`) is negated using the unary `-` operator;
+     *  - If the converted value falls out of range of 32-bit integers, either Int.MaxValue or Int.MinValue is returned;
+     *  - Note that reading the "-MaxValue" is undefined behaviour in C, and so is it in this implementation;
+     *  - If the input doesn't match an integer then no input is consumed.
+     */
+
+    val EOF = -1
+
+    val in = System.in
+    assert(in.markSupported())
+    def markStream() = in.mark(Int.MaxValue)
+
+    // Handle error in parsing and restore the stream, return the given error code
+    def fail(e: Int): Int = {
+      in.reset()
+      e // TODO throw an exception and change `e` for a decent error message
+    }
+
+    // Handle success, but also restore stream in case we peeked at the next character
+    def success(x: Int): Int = {
+      in.reset()
+      x
+    }
+
+    // Mark the stream now, so that in case of formatting error we can cancel the read
+    markStream()
+
+    // Match C99 isspace function: either space (0x20), form feed (0x0c), line feed (0x0a), carriage return (0x0d), horizontal tab (0x09) or vertical tab (0x0b)
+    def isSpace(c: Int): Boolean = Set(0x20, 0x0c, 0x0a, 0x0d, 0x09, 0x0b) contains c
+
+    // Digit base 10
+    def isDigit(c: Int): Boolean = c >= '0' && c <= '9'
+
+    // Return the first non-space character, return -1 if reach EOF
+    def skipSpaces(): Int = {
+      val x = in.read()
+      if (isSpace(x)) skipSpaces()
+      else            x
+    }
+
+    // Safely wrap the addition of the accumulator with a digit character
+    def safeAdd(acc: Int, c: Int): Int = {
+      require(isDigit(c))
+
+      val x = c - '0'
+      val r = acc * 10 + x
+
+      if (r >= 0) r
+      else        Int.MaxValue
+    } // ensuring { res => res >= 0 && res <= Int.MaxInt }
+
+    // Read as many digit as possible, and after each digit we mark
+    // the stream to simulate a "peek" at the next, possibly non-digit,
+    // character on the stream.
+    def readDecInt(acc: Int, mark: Boolean): Int = {
+      if (mark) markStream()
+
+      val c = in.read()
+
+      if (isDigit(c)) readDecInt(safeAdd(acc, c), true)
+      else if (mark)  success(acc) // at least one digit was processed
+      else            fail(-2) // no digit was processed
+    }
+
+    val first = skipSpaces()
+    first match {
+      case EOF             => fail(-1)
+      case '-'             => - readDecInt(0, false)
+      case '+'             =>   readDecInt(0, false)
+      case c if isDigit(c) =>   readDecInt(c - '0', true)
+      case _               => fail(-3)
+    }
   } ensuring((x: Int) => true)
 
   @library
