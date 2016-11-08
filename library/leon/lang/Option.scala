b18fef02c77078d6218971cd194c77c4dce75356
Marco Antognini <antognini.marco@gmail.com>
2016-11-08 12:10:59 +0100

    Inline most of Option's methods
    This will allow methods asking function as parameter to be used in GenC thanks to inlining.

--- library/leon/lang/Option.scala
+++ library/leon/lang/Option.scala
@@ -16,11 +16,13 @@ sealed abstract class Option[T] {
     }
   }
 
+  @inline
   def getOrElse(default: =>T) = this match {
     case Some(v) => v
     case None()  => default
   }
 
+  @inline
   def orElse(or: =>Option[T]) = { this match {
     case Some(v) => this
     case None() => or
@@ -39,30 +41,36 @@ sealed abstract class Option[T] {
 
 
   // Higher-order API
+  @inline
   @isabelle.function(term = "%x f. Option.map_option f x")
   def map[R](f: T => R) = { this match {
     case None() => None[R]()
     case Some(x) => Some(f(x))
   }} ensuring { _.isDefined == this.isDefined }
 
+  @inline
   @isabelle.function(term = "Option.bind")
   def flatMap[R](f: T => Option[R]) = this match {
     case None() => None[R]()
     case Some(x) => f(x)
   }
 
+  @inline
   def filter(p: T => Boolean) = this match {
     case Some(x) if p(x) => Some(x)
     case _ => None[T]()
   }
 
+  @inline
   def withFilter(p: T => Boolean) = filter(p)
 
+  @inline
   def forall(p: T => Boolean) = this match {
-    case Some(x) if !p(x) => false 
+    case Some(x) if !p(x) => false
     case _ => true
   }
 
+  @inline
   def exists(p: T => Boolean) = !forall(!p(_))
 
   // Transformation to other collections
@@ -70,7 +78,7 @@ sealed abstract class Option[T] {
     case None() => Nil[T]()
     case Some(x) => List(x)
   }
-  
+
   def toSet: Set[T] = this match {
     case None() => Set[T]()
     case Some(x) => Set(x)
