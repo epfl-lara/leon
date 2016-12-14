b84dca9b15887fa4b828a51b38974f1f5bbbafb5
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2016-12-14 17:48:02 +0100

    Remove old code

--- src/main/scala/leon/grammars/enumerators/GrammarEnumerator.scala
+++ src/main/scala/leon/grammars/enumerators/GrammarEnumerator.scala
@@ -2,8 +2,6 @@ package leon
 package grammars
 package enumerators
 
-import leon.grammars.enumerators.CandidateScorer.MeetsSpec
-import leon.synthesis.Example
 import purescala.Expressions.Expr
 
 import scala.collection.mutable.{HashMap, HashSet, Queue => MutableQueue}
@@ -59,46 +57,4 @@ object GrammarEnumerator {
     map.toMap
   }
 
-}
-
-object Tester {
-  /********** TEST CODE **********/
-  import leon.frontends.scalac.ExtractionPhase
-  import leon.synthesis.{SynthesisSettings, SynthesisContext}
-  import leon.utils.PreprocessingPhase
-  import leon.purescala.Types._
-
-  def main(args: Array[String]) = {
-    val pipeline =  ExtractionPhase andThen new PreprocessingPhase
-    implicit val (ctx, program) = pipeline.run(
-      LeonContext.empty,
-      List("/home/koukouto/Documents/Leon/testcases/synthesis/userdefined/Grammar.scala")
-    )
-    val fd = program.definedFunctions.find(_.id.name == "max").get
-    val sctx = new SynthesisContext(ctx, SynthesisSettings(), fd, program)
-    val grammar = UserDefinedGrammar(sctx, program, Some(fd), fd.paramIds)
-    val labels = List(IntegerType, BooleanType) map (Label(_, List()))//aspects.Tagged(Tags.Top, 0, None))))
-    val bottomUp = new ProbwiseBottomupEnumerator(grammar, labels(0)).iterator(labels(0))
-    val scorer = new CandidateScorer[Expr]((_, _) => MeetsSpec.NOTYET, _ => Seq(), _.falseProduce(nt => ExpansionExpr(nt, Untyped)))
-    val topDown = new ProbwiseTopdownEnumerator(grammar, labels(0), scorer).iterator(labels(0))
-    grammar.printProductions(println)
-    val before = System.currentTimeMillis()
-
-    /** Code for comparing **/
-    //val b0 = for(_ <- 1 to 100) yield bottomUp.next
-    //val t0 = for(_ <- 1 to 100) yield topDown.next
-    //b0 zip t0 foreach { case ((b1, b2), (t1, t2)) =>
-    //  println(f"$b1%60s: $b2%3.3f vs $t1%60s: $t2%3.3f")
-    //}
-
-    /** Code for bottom-up **/
-    for (label <- labels; i <- 1 to 100000 ) {
-      if (bottomUp.hasNext) {
-        val e = bottomUp.next
-        if (i%20000 == 0)
-          println(f"$i%7d: ${e.asString}")
-      }
-    }
-    println(s"Time: ${System.currentTimeMillis() - before}")
-  }
 }
