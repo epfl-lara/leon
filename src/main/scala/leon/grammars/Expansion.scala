1037a36f9aec6fe7ca99e89c1aa90b1d2b955824
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2016-12-15 15:51:24 +0100

    Remove false produce & small fixes

--- src/main/scala/leon/grammars/Expansion.scala
+++ src/main/scala/leon/grammars/Expansion.scala
@@ -8,8 +8,8 @@ import leon.purescala.Types.TypeTree
 
 /**
  * Represents a (partial) expansion of the rules of the grammar
-  *
-  * @param nt: Non-terminal being expanded at the head
+ *
+ * @param nt: Non-terminal being expanded at the head
  * @tparam NT The type of non-terminal symbols of the grammar
  * @tparam R The type of syntax trees of the grammar
  */
@@ -21,14 +21,9 @@ sealed abstract class Expansion[NT, R](val nt: NT) {
   /**
    * Produces the expansion.
     *
-    * @throws `NoSuchElementException` if the expansion is not complete
+    * @throws `UnsupportedOperationException` if the expansion is not complete
    */
   def produce: R
-
-  /**
-    * Produces the expansion, and wraps instances of NonTerminalInstance using ntWrap
-    */
-  def falseProduce(ntWrap: NonTerminalInstance[NT, R] => R): R
 
   /**
    * Size of the expansion
@@ -36,7 +31,6 @@ sealed abstract class Expansion[NT, R](val nt: NT) {
   def size: Int
 
   /**
-    *
     * Computes the ``horizon'' of this partial expansion. The horizon is the minimum extra log probability of all
     * completed extensions of this expansion.
     * @param nthor The horizon of each non-terminal
@@ -46,28 +40,23 @@ sealed abstract class Expansion[NT, R](val nt: NT) {
 }
 
 case class NonTerminalInstance[NT, R](override val nt: NT) extends Expansion[NT, R](nt) {
-  override val complete: Boolean = false
-  override def produce: R = throw new NoSuchElementException(s"Unable to expand non-terminal ${this}")
-  override def falseProduce(ntWrap: NonTerminalInstance[NT, R] => R): R = ntWrap(this)
-  override val size: Int = 1
-  override def horizon(nthor: NT => Double): Double = nthor(nt)
+  val complete: Boolean = false
+  def produce: R = throw new UnsupportedOperationException(s"Unable to expand non-terminal ${this}")
+  val size: Int = 1
+  def horizon(nthor: NT => Double): Double = nthor(nt)
 }
 
 case class ProdRuleInstance[NT, R](
-    override val nt: NT,
-    rule: ProductionRule[NT, R],
-    children: List[Expansion[NT, R]]
+  override val nt: NT,
+  rule: ProductionRule[NT, R],
+  children: List[Expansion[NT, R]]
 ) extends Expansion[NT, R](nt) {
 
   require(children.map(_.nt) == rule.subTrees)
-  override val complete: Boolean = children.forall(_.complete)
-  override def produce: R = rule.builder(children.map(_.produce))
-  override def falseProduce(ntWrap: NonTerminalInstance[NT, R] => R): R = {
-    rule.builder(children.map(_.falseProduce(ntWrap)))
-  }
-  override val size: Int = 1 + children.map(_.size).sum
-  override def horizon(nthor: NT => Double): Double = children.map(c => c.horizon(nthor)).sum
-
+  val complete: Boolean = children.forall(_.complete)
+  def produce: R = rule.builder(children.map(_.produce))
+  val size: Int = 1 + children.map(_.size).sum
+  def horizon(nthor: NT => Double): Double = children.map(c => c.horizon(nthor)).sum
 }
 
 /**
@@ -78,7 +67,8 @@ case class ProdRuleInstance[NT, R](
   */
 case class ExpansionExpr(expansion: Expansion[Label, Expr])
   extends Expr with Terminal with PrettyPrintable {
-  override def getType: TypeTree = {
+
+  def getType: TypeTree = {
     expansion.nt.getType
   }
 
@@ -89,9 +79,12 @@ case class ExpansionExpr(expansion: Expansion[Label, Expr])
 
   override def isSimpleExpr = true
 
-  override def toString = expansion.falseProduce { nti =>
-    val tp = nti.nt.getType
-    FreshIdentifier(Console.BOLD + tp.toString + Console.RESET, tp).toVariable
-  }.toString
+  override def toString = try {
+    expansion.produce.toString
+  } catch {
+    case _: UnsupportedOperationException =>
+      val tp = expansion.nt.getType
+      FreshIdentifier(Console.BOLD + tp.toString + Console.RESET, tp).toVariable.toString
+  }
 
 }
