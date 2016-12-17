d9d28c9ad198a678751b1f0bf2bed5c12ca7f5ac
Ravi <ravi.kandhadai@epfl.ch>
2016-12-17 10:35:00 +0100

    Updating the red-black tree to use a bound with log

--- testcases/web/resourcebounds/09_RedBlackTree.scala
+++ testcases/web/resourcebounds/09_RedBlackTree.scala
@@ -1,6 +1,7 @@
 import leon.invariant._
 import leon.instrumentation._
 import scala.collection.immutable.Set
+import leon.lang._
 
 object RedBlackTree {
   sealed abstract class Color
@@ -13,10 +14,33 @@ object RedBlackTree {
 
   def twopower(x: BigInt) : BigInt = {
     require(x >= 0)
-    if(x < 1) 1
+    if(x <= 0) BigInt(1)
     else
       2* twopower(x - 1)
-  }
+  } ensuring(_ >= 1)
+
+  // uses integer division and equivalent to floor(log(x))
+  def log(x: BigInt): BigInt = {
+    require(x >= 1)
+    if (x <= 1) BigInt(0)
+    else 1 + log(x/2)
+  } ensuring(_ >= 0)
+
+  def logMonotone(x: BigInt, y: BigInt): Boolean = {
+    require(x >= y && y >= 1)
+    (if(y <= 1) true else logMonotone(x/2, y/2))  && 
+      (log(x) >= log(y)) 
+  } holds
+
+  /**
+  * Proof of the axiom: log(2^x) = x
+  **/
+  def logTwopowerAxiom(x: BigInt): Boolean =  {
+    require(x >= 0)
+    (if(x <= 0) true
+    else logTwopowerAxiom(x - 1)) && log(twopower(x)) == x
+  } holds  
+  
 
   def size(t: Tree): BigInt = {
     require(blackBalanced(t))
@@ -24,16 +48,29 @@ object RedBlackTree {
       case Empty() => BigInt(0)
       case Node(_, l, v, r) => size(l) + 1 + size(r)
     })
-  } ensuring (res => twopower(blackHeight(t)) <= ? *res + ?)
+  } ensuring (res => twopower(blackHeight(t)) <= res + 1)
+
+  /**
+  * Proof of the property that "blackHeight(t) <= log(size(t)+1)"
+  **/
+  def sizeBlackheightProp(t: Tree): Boolean = {
+    require(blackBalanced(t))
+    val bh = blackHeight(t)
+    val sz =  size(t)+1
+    logTwopowerAxiom(bh) &&
+      logMonotone(sz, twopower(bh)) && 
+      bh <= log(sz)
+  } holds
 
   def blackHeight(t : Tree) : BigInt = {
    t match {
     case Node(Black(), l, _, _) => blackHeight(l) + 1
     case Node(Red(), l, _, _) => blackHeight(l)
-    case _ => 0
+    case _ => BigInt(0)
    	}
-  }
+  } ensuring(_ >= 0)
 
+   //We consider leaves to be black
   def isBlack(t: Tree) : Boolean = t match {
     case Empty() => true
     case Node(Black(),_,_,_) => true
@@ -56,26 +93,26 @@ object RedBlackTree {
     case Node(_,l,_,r) => blackBalanced(l) && blackBalanced(r) && blackHeight(l) == blackHeight(r)
     case _ => true
   }
-
-  def ins(x: BigInt, t: Tree): Tree = {
+  
+  def insRec(x: BigInt, t: Tree): Tree = {
     require(redNodesHaveBlackChildren(t) && blackBalanced(t))
 
     t match {
       case Empty() => Node(Red(),Empty(),x,Empty())
       case Node(c,a,y,b) =>
         if(x < y) {
-        	val t1 = ins(x, a)
+        	val t1 = insRec(x, a)
         	balance(c, t1, y, b)
         }
         else if (x == y){
         	Node(c,a,y,b)
         }
         else{
-          val t1 = ins(x, b)
+          val t1 = insRec(x, b)
           balance(c,a,y,t1)
         }
     }
-  } ensuring(res => steps <= ? *blackHeight(t) + ?)
+  } ensuring(res => steps <= ? * blackHeight(t) + ?)
 
   def makeBlack(n: Tree): Tree = {
     n match {
@@ -84,12 +121,15 @@ object RedBlackTree {
     }
   }
 
-  def add(x: BigInt, t: Tree): Tree = {
-    require(redNodesHaveBlackChildren(t) && blackBalanced(t) )
-    val t1 =  ins(x, t)
+  // <<insert element x BigInto the tree t>>
+  def ins(x: BigInt, t: Tree): Tree = {
+    require(redNodesHaveBlackChildren(t) && blackBalanced(t) && 
+      sizeBlackheightProp(t)) // this is an axiom instantiation and is always true 
+
+    val t1 =  insRec(x, t)
     makeBlack(t1)
 
-  } ensuring(res => steps <= ? *blackHeight(t) + ?)
+  } ensuring(res =>  steps <= ? * log(size(t)+1) + ?)
 
   def balance(co: Color, l: Tree, x: BigInt, r: Tree): Tree = {
     Node(co,l,x,r)
@@ -105,4 +145,6 @@ object RedBlackTree {
       case _ => Node(co,l,x,r)
     }
   }
+
+
 }
