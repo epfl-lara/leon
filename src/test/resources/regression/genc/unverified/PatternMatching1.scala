3b4a00a10eacfc87f0c365b4db059cacaea04357
Marco Antognini <antognini.marco@gmail.com>
2017-04-19 15:53:45 +0200

    Add additional example for pattern matching
    This example is currently and incorrectly rejected by Leon's
    verification phase, claiming that the main function doesn't return 0.
    However, executing the programs using Scala shows that it indeed should
    return 0. This need to be fixed.

---
+++ src/test/resources/regression/genc/unverified/PatternMatching1.scala
@@ -0,0 +1,33 @@
+/* Copyright 2009-2016 EPFL, Lausanne */
+
+import leon.annotation._
+import leon.lang._
+
+// This test is "unverified" because side-effect in pat-mat expression
+// is not well supported by the verification system.
+object PatternMatching1 {
+
+  case class Wrapper(var x: Int)
+
+  def _main(): Int = {
+    var c = 1
+
+    def get0(): Int = {
+      c -= 1
+      0
+    }
+
+    val array = Array(Wrapper(42))
+    array(get0()) match {
+      case w if w.x == 42 => w.x = 0
+      case w => w.x = -1
+    }
+
+    array(0).x + c
+  } ensuring { _ == 0 }
+
+  @extern
+  def main(args: Array[String]): Unit = _main()
+
+}
+
